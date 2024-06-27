4125f17b7ed629e161d08526b78d554b452fda9e
Arnavgits <dwivediarnav20@gmail.com>
2024-06-27 18:02:29 +0000

    Thu, Jun 27, 2024, 11:32 PM +05:30

--- mario2.c
+++ mario2.c
@@ -5,7 +5,7 @@ int main(void)
     int n = 4;
     for(int i = 0 ; i < n; i++)
     {
-        for(int j = 0 ; j < n; i++)
+        for(int j = 0 ; j < n; j++)
         {
             printf("#");
         }
