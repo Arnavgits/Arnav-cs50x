ad0741f04a8ce53d6c0c5bd53ad89f37c0c6964d
Arnavgits <dwivediarnav20@gmail.com>
2024-06-27 11:15:55 +0000

    Thu, Jun 27, 2024, 4:45 PM +05:30

--- bhau3.c
+++ bhau3.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 void bhau(void);
-{
-    printf("bhau\n");
-    }
-    int main(void)
+
+int main(void)
 {
     for(i=0; i < 3 ; i-- )
     {
      bhau();
     }
 }
+
+void bhau(void);
+{
+    printf("bhau\n");
+}
+
 
