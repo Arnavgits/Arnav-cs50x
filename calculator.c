83723cf903b8760da58e264c79d064ae99b2ce62
Arnavgits <dwivediarnav20@gmail.com>
2024-06-27 17:19:56 +0000

    Thu, Jun 27, 2024, 10:49 PM +05:30

--- calculator.c
+++ calculator.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int add(void);
+
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
     printf("%i\n", x + y);
+}
+int add(void);
+{
+    
 }
