71886162b6d5b5cf797828e4dbca730daa476d82
Arnavgits <dwivediarnav20@gmail.com>
2024-06-28 08:28:52 +0000

    Fri, Jun 28, 2024, 1:58 PM +05:30

--- mario4.c
+++ mario4.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+    //prompt user for number greater than 0
     int n;
     do
     {
@@ -10,6 +11,7 @@ int main(void)
     }
     while (n<3);
 
+    //
     for (int i = 0;i < n; i++)
     {
         for(int j = 0;j<n; j++)
