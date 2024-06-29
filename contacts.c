4592fa30626f84aa6d1fdf0bf42c7020377ead35
Arnavgits <dwivediarnav20@gmail.com>
2024-06-29 08:12:44 +0000

    Sat, Jun 29, 2024, 1:42 PM +05:30

--- contacts.c
+++ contacts.c
@@ -7,6 +7,7 @@ int main(void)
     int age  = get_int("what is your age?\n");
     int phone no = get_int("Whta is your Phone no?\n");
 
-    printf("Hello, %s\n", name");
-    printf("Your age is
+    printf("Hello, %s\n", name);
+    printf("Your age is %i\n", age);
+    printf("Your Phone no. is %i\n", phone no);
 }
