7ba6349aeebb20bc00382ebbaf42d65f0e4936d3
Arnavgits <dwivediarnav20@gmail.com>
2024-06-29 08:10:23 +0000

    Sat, Jun 29, 2024, 1:40 PM +05:30

--- contacts.c
+++ contacts.c
@@ -3,5 +3,10 @@
 
 int main(void)
 {
-    get string = 
+    string name = get_string("what is your name?.\n");
+    int age  = get_int("what is your age?\n");
+    int phone no = get_int("Whta is your Phone no?\n");
+
+    printf("Hello, %s\n", name");
+    printf("Your age is
 }
