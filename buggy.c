1891012172af894a1122fb9d7b71a20682bfc0f0
Arnavgits <dwivediarnav20@gmail.com>
2024-07-06 04:42:50 +0000

    Sat, Jul 6, 2024, 10:12 AM +05:30

--- buggy.c
+++ buggy.c
@@ -1,16 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_column(int height);
+void print_column(int n);
 int main(void)
 {
     int h = get_int("Height: ");
-    print_column(h);
+    print_column(n);
 }
 
-void print_column(int height)
+void print_column(int n)
 {
-    for(int i= 0; i < height ; i++)
+    for(int i= 0; i < n ; i++)
     {
         printf("#\n");
     }
