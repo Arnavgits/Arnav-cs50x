d621122132a46c80c0c8d114ab6aa1ba394be944
Arnavgits <dwivediarnav20@gmail.com>
2024-07-09 16:11:39 +0000

    Tue, Jul 9, 2024, 9:41 PM +05:30

--- string.c
+++ string.c
@@ -3,6 +3,12 @@
 
 int main(int argc, string argv[])
 {
-
-    printf("hello, %s\n", argv[]);
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
 }
