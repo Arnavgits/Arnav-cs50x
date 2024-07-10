0e4120d9d7961ff9c01992fab1588751b11655bf
Arnavgits <dwivediarnav20@gmail.com>
2024-07-10 11:36:29 +0000

    Wed, Jul 10, 2024, 5:06 PM +05:30

--- array.c
+++ array.c
@@ -5,7 +5,7 @@ int main(void)
 {
     int doble[5];
 
-    for(int doble[0] = 1,int i = 0; i < 5; i++)
+    for(doble[0] = 1,int i = 0; i < 5; i++)
     {
         doble[i] = doble[i - 1] * 2;
         printf("%i\n", doble[i]);
