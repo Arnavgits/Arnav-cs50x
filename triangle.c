5f41e2aa5ffe5fbb52b686725dcaf4c44df65187
Arnavgits <dwivediarnav20@gmail.com>
2024-07-11 12:57:37 +0000

    Thu, Jul 11, 2024, 6:27 PM +05:30

--- triangle.c
+++ triangle.c
@@ -10,5 +10,11 @@ int main(void)
 
 valid_triangle(float a ,float b,float c)
 {
-    float sum_two_sides = 
+    float sum_two_sides = a + b;
+    return sum_two_sides;
+
+    if(sum_two_sides > c)
+    {
+        
+    }
 }
