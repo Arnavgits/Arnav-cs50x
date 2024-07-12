1075bc879dc8efc2b65a238ac0ba880f408103bd
Arnavgits <dwivediarnav20@gmail.com>
2024-07-12 07:26:36 +0000

    Fri, Jul 12, 2024, 12:56 PM +05:30

--- double/doubling.c
+++ double/doubling.c
@@ -6,7 +6,7 @@ int main(void)
     int a = get_int("enter your no: ");
     int arr[a];
     arr[0] = 1;
-    printf("%i", arr[0]);
+    printf("%i\n", arr[0]);
 
     for(int i = 1; i<= a; i++)
     {
