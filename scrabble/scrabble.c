2ceea492578877d19360f516c417c091ef309bbe
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 06:15:35 +0000

    Sat, Jul 13, 2024, 11:45 AM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -9,6 +9,7 @@ int main(void)
 
     scrab[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10}
     // get two input from user
+
     string a = get_string("player 1: ");
     string b = get_string("player 2: ");
 
