5139aaefe25308e58b092e2d63652ab9afab3a82
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 07:06:45 +0000

    Sat, Jul 13, 2024, 12:36 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int scrab[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+int scrab[] = {1,	3,	3,	2,	1,	4,	2,	4,	1,	8,	5,	1,	3,	1,	1,	3,	10,	1,	1,	1,	1,	4,	4,	8,	4,	10};
 
 int compute_value(string c);
 int main(void)
@@ -16,11 +16,11 @@ int main(void)
     int score1 = compute_value(a);
     int score2 = compute_value(b);
 
-    if(score1 > score2)
+    if (score1 > score2)
     {
         printf("player 1 wins\n");
     }
-    else if(score2 > score1)
+    else if (score2 > score1)
     {
         printf("player 2 wins\n");
     }
@@ -32,17 +32,17 @@ int main(void)
 
 int compute_value(string c)
 {
-   int score = 0;
-   for (int i = 0;i < strlen(c); i++)
-   {
-      if(isupper(c[i]))
-      {
-      score = score + scrab[c[i] - 65];
-      }
-      else if(islower(c[i]))
-      {
-      score = score + scrab[c[i] - 97];
-      }
-   }
-   return score;
+    int score = 0;
+    for (int i = 0; i < strlen(c); i++)
+    {
+        if (isupper(c[i]))
+        {
+            score = score + scrab[c[i] - 65];
+        }
+        else if (islower(c[i]))
+        {
+            score = score + scrab[c[i] - 97];
+        }
+    }
+    return score;
 }
