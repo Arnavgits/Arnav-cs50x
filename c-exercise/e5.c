a80a88dea49bd0f45adddc9656a1ad4d850c1955
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 16:24:54 +0000

    Sat, Jul 13, 2024, 9:54 PM +05:30

--- c-exercise/e5.c
+++ c-exercise/e5.c
@@ -3,5 +3,9 @@
 
 int main(void)
 {
-    
+    int a = get_int("Total Dis.(in km): ");
+    float b = get_int("Total fue spent(in litres): ");
+
+    float c = a / b;
+    printf("Average Consumption(km/lt): %f", c);
 }
