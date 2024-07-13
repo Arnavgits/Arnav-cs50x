f12386d8e0b96c25dc236348527e9c22ab5c4ddd
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 07:39:04 +0000

    Sat, Jul 13, 2024, 1:09 PM +05:30

--- c-exercise/e2.c
+++ c-exercise/e2.c
@@ -5,6 +5,11 @@ int main(void)
 {
     //get weight of 2 items from the user and no of those two items purchased
     float item_1_weight = get_float("Weight - Item1: ");
-    float item_2_weight = get_float("Weight - Item1: ");
+    int item_1_number = get_float("No. of item1: ");
+    float item_2_weight = get_float("Weight - Item2: ");
+    int item_2_number = get_float("No. of item2: ");
     
+
+
+
 }
