beb3791829ed1b0de959214cdc6c9cef8f82e841
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 16:27:45 +0000

    Sat, Jul 13, 2024, 9:57 PM +05:30

--- c-exercise/e5.c
+++ c-exercise/e5.c
@@ -4,8 +4,8 @@
 int main(void)
 {
     int a = get_int("Total Dis.(in km): ");
-    float b = get_int("Total fuel spent(in litres): ");
+    float b = get_float("Total fuel spent(in litres): ");
 
     float c = a / b;
-    printf("Average Consumption(km/lt): %f", c);
+    printf("Average Consumption(km/lt): %f\n", c);
 }
