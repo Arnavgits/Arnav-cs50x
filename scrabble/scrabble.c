19bdea0d86c5af1bba727ec75de1704b9c51d919
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 06:10:13 +0000

    Sat, Jul 13, 2024, 11:40 AM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,24 +3,33 @@
 
 int main(void)
 {
+    
     // get two input from user
     string a = get_string("player 1: ");
     string b = get_string("player 2: ");
 
-    int a = compute_value(word)
+    int score1 = compute_value(a);
+    int score2 = compute_value(b);
 
-
-
-
-
-
-
+    if(score1 > score2)
+    {
+        printf("player 1 wins\n");
+    }
+    else if(score2 > score1)
+    {
+        printf("player 2 wins\n");
+    }
+    else
+    {
+        printf("Tie!!");
+    }
     //print out "player 1 wins" or "player 2" wins.
 }
 
 int compute_value(string c)
 {
-     // operate on the two values and compare which is greater
+   // operate on the two values and compare which is greater
+
 
 
 
