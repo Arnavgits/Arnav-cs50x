ade9642d4989c6517f2662a7afa8302d563fc693
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 08:03:48 +0000

    Sat, Jul 13, 2024, 1:33 PM +05:30

--- c-exercise/e3.c
+++ c-exercise/e3.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    long id[10] = get_long("Input the Employees ID(Max. 10 chars): ");
+    long d[10] = get_long("Input the Employees ID(Max. 10 chars): ");
     int hours = get_int("Input the working hrs: ");
     int salary = get_int("Salary amount/hr: ")
 }
