de57e2f7691ff4d49d332a91f853816ead5030e0
Arnavgits <dwivediarnav20@gmail.com>
2024-07-13 17:11:36 +0000

    Sat, Jul 13, 2024, 10:41 PM +05:30

---
+++ c-exercise/e6.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int a = get_int("x1: ");
+    int b = get_int("y1: ");
+    int c = get_int("x2: ");
+    int d = get_int("y2: ");
+
+    
+
+}
