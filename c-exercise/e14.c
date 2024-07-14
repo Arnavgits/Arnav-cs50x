6e70ab032a1701d699f495779317e12cf6a18b7e
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 17:11:59 +0000

    Sun, Jul 14, 2024, 10:41 PM +05:30

--- c-exercise/e14.c
+++ c-exercise/e14.c
@@ -3,5 +3,20 @@
 
 int main(void)
 {
-    
+    // get three floating point values from user
+
+    float a = get_float("Enter first side value: ");
+    float b = get_float("Enter second side value: ");
+    float c = get_float("Enter third side value: ");
+
+    //check if triangle can be made
+    if(a + b > c && b + c > a && c + a > b)
+    {
+        int d = a + b + c;
+        printf("Perimeter = %i\n", d);
+    }
+    else
+    {
+        printf("Triangle cannot be formed\n");
+    }
 }
