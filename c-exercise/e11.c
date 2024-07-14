908ab2973a58b71d48db3f925d4e03aa814a1bea
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 11:42:59 +0000

    Sun, Jul 14, 2024, 5:12 PM +05:30

--- c-exercise/e11.c
+++ c-exercise/e11.c
@@ -8,8 +8,8 @@ int main(void)
     int b = get_int("b: ");
     int c = get_int("c: ");
 
-    float d = ( -a + sqrt(pow(b , 2) - 4*a*c)) / 2 * a;
-    float e = ( -a - sqrt(pow(b , 2) - 4*a*c)) / 2 * a;
+    float d = ( -b + sqrt(pow(b , 2) - 4*a*c)) / (2 * a);
+    float e = ( -b - sqrt(pow(b , 2) - 4*a*c)) / (2 * a);
 
     printf("Root1: %f\nRoot2: %f\n", d, e);
 }
