be423314aae3c76b6ecff34207b12f0409881e56
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 16:43:49 +0000

    Sun, Jul 14, 2024, 10:13 PM +05:30

---
+++ c-exercise/e13.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int a = get_int("Enter forst no: ");
+    int b = get_int("Enter 2nd no: ");
+    int c = get_int("Enter 3rd no: ");
+    int d = get_int("Enter 4th no: ");
+    int e = get_int("Enter 5th no: ");
+
+    
+}
