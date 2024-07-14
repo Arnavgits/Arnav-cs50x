ecf078a98700061bdd0ca4d0f240f7401df520e9
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 16:55:19 +0000

    Sun, Jul 14, 2024, 10:25 PM +05:30

--- c-exercise/e13.c
+++ c-exercise/e13.c
@@ -4,11 +4,16 @@
 int main(void)
 {
     int a[5];
-    for(int i = 1;i <= 5; i++)
+    for(int i = 0;i <= 5; i++)
     {
         a[i] = get_int("Enter %i no: ", i);
     }
 
-    
+    int b = 0;
+
+    for(i = 0; i < 5; i++)
+    {
+
+    }
 
 }
