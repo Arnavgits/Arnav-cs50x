92d45dd7ca7facc57af898efaf0272495b3c0390
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 11:39:49 +0000

    Sun, Jul 14, 2024, 5:09 PM +05:30

--- c-exercise/e11.c
+++ c-exercise/e11.c
@@ -3,5 +3,12 @@
 
 int main(void)
 {
-    
+    int a = get_int("a: ");
+    int b = get_int("b: ");
+    int c = get_int("c: ");
+
+    float d = ( -a + sqrt(pow(b , 2) - 4*a*c)) / 2 * a;
+    float e = ( -a - sqrt(pow(b , 2) - 4*a*c)) / 2 * a;
+
+    printf("Root1: %i\nRoot2: %i", d, e);
 }
