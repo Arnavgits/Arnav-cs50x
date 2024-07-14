bafd97c8afbf78f2cc21c9e08f6adfd491060ef0
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 08:48:34 +0000

    Sun, Jul 14, 2024, 2:18 PM +05:30

--- c-exercise/e8.c
+++ c-exercise/e8.c
@@ -4,5 +4,15 @@
 int main(void)
 {
     int a = get_int("Enter Seconds: ");
-    
+
+    int hours = a / 60;
+    a = a - hours * 60;
+
+    int min = a / 60;
+    a = a - min * 60;
+
+    int sec = a / 60;
+    a = a - sec * 60;
+
+    printf("There are: H:M:S - %i:%i:%i\n",hours ,min ,sec);
 }
