0debd9b99bce6a4290d3b892f2ac607e6280afad
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 08:40:58 +0000

    Sun, Jul 14, 2024, 2:10 PM +05:30

--- c-exercise/e7.c
+++ c-exercise/e7.c
@@ -7,40 +7,36 @@ int main(void)
 
     int b = a / 100;
     printf("%i Note(s) of 100.00 \n", b);
-
     a = a - b * 100;
-    return a;
 
     int c = a / 50;
-    printf("%i Note(s) of 50.00", c);
-
+    printf("%i Note(s) of 50.00\n", c);
     a = a - c * 50;
-    return a;
 
     int d = a / 20;
-    printf("%i Note(s) of 20.00", d);
+    printf("%i Note(s) of 20.00\n", d);
 
     a = a - d * 20;
-    return a;
+
 
     int e = a /  10;
-    printf("%i Note(s) of 10.00", e);
+    printf("%i Note(s) of 10.00\n", e);
 
     a = a - e * 10;
-    return a;
+
 
     int f = a / 5;
-    printf("%i Note(s) of 5.00", f);
+    printf("%i Note(s) of 5.00\n", f);
 
     a = a - f * 5;
-    return a;
+
 
     int g = a / 2;
-    printf("%i Note(s) of 2.00", g);
+    printf("%i Note(s) of 2.00\n", g);
 
     a = a - g * 2;
-    return a;
+
 
     int h = a / 1;
-    printf("%i Note(s) of 1.00", h);
+    printf("%i Note(s) of 1.00\n", h);
 }
