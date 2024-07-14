46f93db9b2f8474e57de08115d09dca3b6c8374b
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 09:22:20 +0000

    Sun, Jul 14, 2024, 2:52 PM +05:30

--- c-exercise/e10.c
+++ c-exercise/e10.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
+    do
+    {
     int p = get_int("p: ");
+    }
+    while (p % 2 = 0)
+
     unsigned q = get_int("q: ");
     unsigned r = get_int("r: ");
     unsigned s = get_int("s: ");
