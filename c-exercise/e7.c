4d4c1841f63deddd17ce363e6de5ff943c567487
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 08:32:34 +0000

    Sun, Jul 14, 2024, 2:02 PM +05:30

--- c-exercise/e7.c
+++ c-exercise/e7.c
@@ -6,9 +6,36 @@ int main(void)
     int a = get_int("Enter your Amount:");
 
     int b = a / 100;
-    printf("%i Note(s) of 100.00 \n");
+    printf("%i Note(s) of 100.00 \n", b);
 
     a = a - b * 100;
+    return a;
+
+    int c = a / 50;
+    printf("%i Note(s) of 50.00", c);
+
+    a = a - c * 50;
+    return a;
+
+    int d = a / 20;
+    printf("%i Note(s) of 20.00", d);
+
+    a = a - d * 20;
+    return a;
+
+    int e = a /  10;
+    printf("%i Note(s) of 10.00", e);
+
+    a = a - e * 10;
+    return a;
+
+    int f = a / 5;
+    printf("%i Note(s) of 5.00", f);
+
+    a = a - f * 5;
+    retunr a;
+
+    int g = a / 2;
     
 
 
