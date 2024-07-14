759d6bdb10f5eac386d00cce3794049f7732bc74
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 16:34:19 +0000

    Sun, Jul 14, 2024, 10:04 PM +05:30

--- c-exercise/e12.c
+++ c-exercise/e12.c
@@ -3,5 +3,30 @@
 
 int main(void)
 {
-    
+    do
+    {
+    int a = get_int("Enter your no: ");
+    }
+    while(a < 0)
+
+    if(a >= 0 && a <= 20)
+    {
+        printf("Range [0,20]");
+    }
+    else if(a > 20 && a <= 40)
+    {
+        printf("Range (20,40]");
+    }
+    else if(a > 40 && a <= 60)
+    {
+        printf("Range (40,60]");
+    }
+    else if(a > 60 && a <= 80)
+    {
+        printf("Range (60,80]");
+    }
+    else
+    {
+        printf("Error");
+    }
 }
