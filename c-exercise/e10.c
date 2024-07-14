eab9daf0c3e9dc2bd888ef4b074e9f2b4b57d8c3
Arnavgits <dwivediarnav20@gmail.com>
2024-07-14 09:26:38 +0000

    Sun, Jul 14, 2024, 2:56 PM +05:30

--- c-exercise/e10.c
+++ c-exercise/e10.c
@@ -9,7 +9,7 @@ int main(void)
     {
      p = get_int("p: ");
     }
-    while (p % 2 = 0)
+    while (p % 2 == 0)
 
     do
     {
@@ -17,5 +17,5 @@ int main(void)
      r = get_int("r: ");
      s = get_int("s: ");
     }
-    while (q,r,s > 0)
+    while (q> 0)
 }
