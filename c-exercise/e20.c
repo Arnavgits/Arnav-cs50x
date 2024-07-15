dd6ea28243d397a0b1e4e538e55e9b585098c8e9
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 15:40:10 +0000

    Mon, Jul 15, 2024, 9:10 PM +05:30

--- c-exercise/e20.c
+++ c-exercise/e20.c
@@ -3,5 +3,15 @@
 
 int main(void)
 {
-    
+    int a = get_int("Initial no: ");
+    int b = get_int("Ending no: ");
+
+    for(int i = a; i <= b; i++)
+    {
+        if(i % 2 == 0)
+        {
+            
+        }
+    }
+
 }
