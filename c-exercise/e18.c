ea87dc1fa6496a896fbd57157918561e6ce7653a
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 15:22:47 +0000

    Mon, Jul 15, 2024, 8:52 PM +05:30

--- c-exercise/e18.c
+++ c-exercise/e18.c
@@ -3,13 +3,25 @@
 
 int main(void)
 {
-    a[5];
-    int a[0] = get_int("Enter 1st no: ");
-    int a[1] = get_int("Enter 2nd no: ");
-    int a[2] = get_int("Enter 3rd no: ");
-    int a[3] = get_int("Enter 4th no: ");
-    int a[4] = get_int("Enter 5th no: ");
+    int a[5];
+    int b = 0,c = 0;
+     a[0] = get_int("Enter 1st no: ");
+     a[1] = get_int("Enter 2nd no: ");
+     a[2] = get_int("Enter 3rd no: ");
+     a[3] = get_int("Enter 4th no: ");
+     a[4] = get_int("Enter 5th no: ");
 
-    for(i = 0;i < 5; i++)
+    for(int i = 0; i < 5; i++)
+    {
+        if(a[i] >= 0)
+        {
+            b = b + 1 ;
+        }
+        else
+        {
+            c = c + 1 ;
+        }
+    }
+    printf("Positive no: %i\nNegative no: %i\n", b, c);
 
 }
