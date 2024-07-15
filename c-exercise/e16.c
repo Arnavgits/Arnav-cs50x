59b4f4fba97059eb16a1f20ded5c6cdd11ab2dfc
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 14:17:47 +0000

    Mon, Jul 15, 2024, 7:47 PM +05:30

--- c-exercise/e16.c
+++ c-exercise/e16.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int mai(void)
+int main(void)
 {
     int a;
     do
@@ -10,19 +10,19 @@ int mai(void)
     }
     while(a > 0 && a < 13);
 
-    switch(a);
+    switch(a)
 
-    case 1 = printf("January\n");break;
-    case 2 = printf("Feb\n");break;
-    case 3 = printf("March0\n");break;
-    case 4 = printf("april\n");break;
-    case 5 = printf("may\n");break;
-    case 6 = printf("june\n");break;
-    case 7 = printf("july\n");break;
-    case 8 = printf("aug\n");break;
-    case 9 = printf("sep\n");break;
-    case 10 = printf("oct\n");break;
-    case 11 = printf("nov\n");break;
-    case 12 = printf("dec\n");break;
-    default = printf("Enter only between 1 and 12\n");break;
+    case 1 : printf("January\n");break;
+    case 2 : printf("Feb\n");break;
+    case 3 : printf("March0\n");break;
+    case 4 : printf("april\n");break;
+    case 5 : printf("may\n");break;
+    case 6 : printf("june\n");break;
+    case 7 : printf("july\n");break;
+    case 8 : printf("aug\n");break;
+    case 9 : printf("sep\n");break;
+    case 10 : printf("oct\n");break;
+    case 11 : printf("nov\n");break;
+    case 12 : printf("dec\n");break;
+    default : printf("Enter only between 1 and 12\n");break;
 }
