9cb4effcba5fa0c359842ce239c65bee8631f741
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 14:27:02 +0000

    Mon, Jul 15, 2024, 7:57 PM +05:30

--- c-exercise/e16.c
+++ c-exercise/e16.c
@@ -3,27 +3,13 @@
 
 int main(void)
 {
-    int a;
+    int a[] = {J,F,M,A,MAY,JUNE,JULY,aug,dep,oct,nov,dec};
+
     do
     {
-        a = get_int("Input a  number to get the month name: ");
+        a = get_int("eNTER no to get month: ");
     }
     while(a < 0 || a > 13);
 
-    switch(a)
-    {
-    case 1 : printf("January\n");break;
-    case 2 : printf("Feb\n");break;
-    case 3 : printf("March0\n");break;
-    case 4 : printf("april\n");break;
-    case 5 : printf("may\n");break;
-    case 6 : printf("june\n");break;
-    case 7 : printf("july\n");break;
-    case 8 : printf("aug\n");break;
-    case 9 : printf("sep\n");break;
-    case 10 : printf("oct\n");break;
-    case 11 : printf("nov\n");break;
-    case 12 : printf("dec\n");break;
-    default : printf("Enter only between 1 and 12\n");break;
-    }
+    
 }
