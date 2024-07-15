a78c46693cbd8c9116c00bbf6d7c41a936dc40ac
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 15:43:59 +0000

    Mon, Jul 15, 2024, 9:13 PM +05:30

--- c-exercise/e20.c
+++ c-exercise/e20.c
@@ -1,8 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
+    int c;
     int a = get_int("Initial no: ");
     int b = get_int("Ending no: ");
 
@@ -10,7 +12,8 @@ int main(void)
     {
         if(i % 2 == 0)
         {
-            
+            c = pow(i , 2);
+            printf("%i^2 = %i\n",i , c);
         }
     }
 
