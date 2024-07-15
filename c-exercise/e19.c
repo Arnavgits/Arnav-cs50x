5a4aa98552d00f90c76c4f2a10532ea97858530b
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 15:27:55 +0000

    Mon, Jul 15, 2024, 8:57 PM +05:30

--- c-exercise/e19.c
+++ c-exercise/e19.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
-    
+    int a[5];
+
+    a[0] = get_int("Enter 1st no: ");
+    a[1] = get_int("Enter 2nd no: ");
+    a[2] = get_int("Enter 3rd no: ");
+    a[3] = get_int("Enter 4th no: ");
+    a[4] = get_int("Enter 5th no: ");
+
+    for(int i = 0; i < 5; i++)
+    {
+        if(i % 2 != 0)
+        {
+            a[i] = a[i] +
+        }
+    }
 }
