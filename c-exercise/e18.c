bd977ff11157faf70f4ff35526ff26d98b067177
Arnavgits <dwivediarnav20@gmail.com>
2024-07-15 15:13:44 +0000

    Mon, Jul 15, 2024, 8:43 PM +05:30

--- c-exercise/e18.c
+++ c-exercise/e18.c
@@ -3,10 +3,13 @@
 
 int main(void)
 {
-    int a = get_int("Enter 1st no: ");
-    int b = get_int("Enter 2nd no: ");
-    int c = get_int("Enter 3rd no: ");
-    int d = get_int("Enter 4th no: ");
-    int e = get_int("Enter 5th no: ");
-    
+    a[5];
+    int a[0] = get_int("Enter 1st no: ");
+    int a[1] = get_int("Enter 2nd no: ");
+    int a[2] = get_int("Enter 3rd no: ");
+    int a[3] = get_int("Enter 4th no: ");
+    int a[4] = get_int("Enter 5th no: ");
+
+    for(i = 0;i < 5; i++)
+
 }
