1a01095b783a9a16a5904230dcb1c38dbdf010ea
Arnavgits <dwivediarnav20@gmail.com>
2024-07-16 07:52:04 +0000

    Tue, Jul 16, 2024, 1:22 PM +05:30

--- e22.c
+++ e22.c
@@ -5,8 +5,8 @@ int main(void)
 {
     int a[10];
 
-    for(i = 0; i < 10; i++)
+    for(int i = 0; i < 10; i++)
     {
-        a[i] = get_int("element - %i : %i", i, )
+        a[i] = get_int("element - %i : ", i)
     }
 }
