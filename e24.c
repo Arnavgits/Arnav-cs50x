62cb9e5cb6e86c6e7977b58f215fe18029894b74
Arnavgits <dwivediarnav20@gmail.com>
2024-07-16 08:40:02 +0000

    Tue, Jul 16, 2024, 2:10 PM +05:30

--- e24.c
+++ e24.c
@@ -5,11 +5,13 @@ int main(void)
     int a = get_int("How many values to enter :");
 
     int b[a];
+    int c = 0;
 
     for(int i = 0;i < a; i++)
     {
         b[i] = get_int("element - %i: ", i);
+
+        c = c + b[i];
     }
-
-
+    printf("Sum : %i\n", c);
 }
