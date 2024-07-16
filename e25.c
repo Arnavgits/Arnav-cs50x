02318d2d17753742b6e797f1d8d719a930984a21
Arnavgits <dwivediarnav20@gmail.com>
2024-07-16 09:06:53 +0000

    Tue, Jul 16, 2024, 2:36 PM +05:30

--- e25.c
+++ e25.c
@@ -11,5 +11,20 @@ int main(void)
     for(int i = 0; i < a; i++)
     {
         b[i] = get_int("element - %i : ", i );
+        c[i] = b[i];
     }
+
+    printf("Elemets in firs array: ");
+
+    for(int i = 0; i < a; i++);
+    {
+        printf("%i ", b[i]);
+    }
+
+    printf("Elements in 2nd array: ");
+    for(int i = 0; i < a; i++);
+    {
+        printf("%i ", c[i]);
+    }
+
 }
