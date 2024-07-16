55fc2437338af2c52f897f1050bf0be3ab1c4bc7
Arnavgits <dwivediarnav20@gmail.com>
2024-07-16 09:51:43 +0000

    Tue, Jul 16, 2024, 3:21 PM +05:30

--- c-exercise/e26.c
+++ c-exercise/e26.c
@@ -13,13 +13,16 @@ int main(void)
     b[i] = get_int("element- %i: ", i);
    }
 
-   for(int i = 0; i < a ; i++)
+   for(int i = 0; i < a - 1; i++)
    {
-    if(b[i] == b[i + 1])
+   for(int j = 0; j < a - 1; j++)
+   {
+    if(b[i] == b[j] && i != j)
     {
         c = c + 1;
         break;
     }
+   }
    }
 
    printf("%i \n", c);
