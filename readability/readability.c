f4a5318c250f72bd0377bdc7c17c586ed1e657c3
Arnavgits <dwivediarnav20@gmail.com>
2024-07-18 08:15:30 +0000

    Thu, Jul 18, 2024, 1:45 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -13,7 +13,7 @@ int main(void)
 
     for(int i = 0; i < b; i ++)
     {
-        if( a[i] == ' ')
+        if( a[i] == ' ' || a[i] == '.' || a[i] == '!' || a[i] == '?')
         {
             words++;
         }
