9af10d9ddfaeee582109795d99a8f0784c707e2b
Arnavgits <dwivediarnav20@gmail.com>
2024-07-18 11:49:02 +0000

    Thu, Jul 18, 2024, 5:19 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(argc, argv[])
 {
     string plain = get_string("Enter text: ");
     int a = strlen(plain);
    char cipher[strlen(plain)];
+   int k;
 
     for(int i = 0;i < a; i++)
     {
+        if()
         cipher[i] = plain[i] + 2;
          printf("%c", cipher[i]);
     }
