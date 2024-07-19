407923008e0cf74ab5332c97a5367922ccb656dc
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 06:35:51 +0000

    Fri, Jul 19, 2024, 12:05 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,7 +6,6 @@
 
 int main(int argc, string argv[])
 {
-//aceept a single non-negative integer in command line argumemt
      if(argc != 2)
      {
         printf("more or less than 2 command line arguments\n");
@@ -25,7 +24,7 @@ int main(int argc, string argv[])
      int k = atoi(argv[1]);
 
      string plain = get_string("plaintext: ");
-     string cipher[strlen(plain)];
+     char cipher[strlen(plain)];
 
      for(int i = 0; i < strlen(plain); i++)
      {
