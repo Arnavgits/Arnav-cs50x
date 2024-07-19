4423495c70664b71dd3c16ca441f99a91733e436
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 10:46:11 +0000

    Fri, Jul 19, 2024, 4:16 PM +05:30

--- c-w3-resource/c-algorithm/e3.c
+++ c-w3-resource/c-algorithm/e3.c
@@ -6,6 +6,6 @@ int main(void)
     int a = get_int("a: ");
     int b = get_int("b: ");
 
-    int c = (a = 30 || b = 30 || a + b = 30)
-
+    int c = (a == 30 || b == 30 || a + b == 30) ? true : false;
+    printf("output: %i\n", c );
 }
