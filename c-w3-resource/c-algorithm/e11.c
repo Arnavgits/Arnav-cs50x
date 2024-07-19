f0595d8224ccb71362a3ecc5c476f86585144179
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 14:51:32 +0000

    Fri, Jul 19, 2024, 8:21 PM +05:30

--- c-w3-resource/c-algorithm/e11.c
+++ c-w3-resource/c-algorithm/e11.c
@@ -5,11 +5,13 @@ int main(void)
 {
     int a = get_int("No of integers to enter: ");
     int b[a];
+    int c;
 
     for (int i = 0; i < a; i++)
     {
     b[i] = get_int("array element %i: ", i);
-    int c = (b[i] == b[i + 1] == b[i + 2]) ? true : false;
-    printf("%i\n", c);
+    c = (b[i] == b[i + 1] == b[i + 2]) ? true : false;
+    return 0;
     }
+    printf("%i\n", c);
 }
