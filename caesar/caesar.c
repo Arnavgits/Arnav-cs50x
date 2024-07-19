64e164ab5952ab4f65f36b07fa176db31bbd1908
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 06:01:22 +0000

    Fri, Jul 19, 2024, 11:31 AM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,7 +7,7 @@
 int main(int argc, string argv[])
 {
 //aceept a single non-negative integer in command line argumemt
-     if(argc != 2 || argv[1] )
+     if(argc != 2 || isalphanumeric(argv[1]) || ispunct(argv[1]) )
      {
         printf("more or less than 2 command line arguments\n");
         return 1;
