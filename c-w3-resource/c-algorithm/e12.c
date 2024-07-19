84ed3097f62b2011576329d7544403a218593dc2
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 15:39:53 +0000

    Fri, Jul 19, 2024, 9:09 PM +05:30

--- c-w3-resource/c-algorithm/e12.c
+++ c-w3-resource/c-algorithm/e12.c
@@ -1,12 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int mai(void)
+int main(void)
 {
     int a = get_int("a: ");
     int b = get_int("b: ");
     int c = a + b;
-    
+
     if((c <= 20 && c >= 10))
     {
         printf("30\n");
