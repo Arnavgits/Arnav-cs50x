e7c25f9660a5fdb2743bb9fb61259033325a0883
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 11:18:21 +0000

    Fri, Jul 19, 2024, 4:48 PM +05:30

--- c-w3-resource/c-algorithm/e6.c
+++ c-w3-resource/c-algorithm/e6.c
@@ -1,10 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     int a = get_int("a: ");
     int b = get_int("b: ");
 
-    
+    if(abs(a - 100) > abs(b - 100))
+    {
+        
+    }
 }
