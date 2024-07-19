c6691bb4424b20c6988d8da884f9beb82030a431
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 05:58:40 +0000

    Fri, Jul 19, 2024, 11:28 AM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,36 +7,10 @@
 int main(int argc, string argv[])
 {
 //aceept a single non-negative integer in command line argumemt
-     if(argc != 2)
+     if(argc != 2 || argv[1] )
      {
-        printf("more or less than 2 command line arguments");
+        printf("more or less than 2 command line arguments\n");
         return 1;
      }
-     else if()
-     {
-        return 0;
-     }
-     else
-     {
-        printf("Usage: ./caesar key");
-     }
 
-
-    for(int i = 0;i < a; i++)
-    {
-        if(isupper(plain[i]))
-        {
-             cipher[i] = (plain[i] + k) % 26 + 'A';
-             printf("%c", cipher[i]);
-        }
-        else if(islower(plain[i]))
-        {
-            cipher[i] = (plain[i] + k) % 26 + 'a';
-            printf("%c", cipher[i]);
-        }
-        else
-        {
-            printf("invalid text");
-        }
-    }
 }
