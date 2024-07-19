85e89001d4ff424c20df699ff6785eff719beb0a
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 10:25:45 +0000

    Fri, Jul 19, 2024, 3:55 PM +05:30

---
+++ c-w3-resource/c-algorithm/e1.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int a = get_int("Input a: ");
+    int b = get_int("Input b: ");
+
+    if( a == b )
+    int c = a + b;
+    printf("Output: %i", 3 * c);
+    else
+        printf("Output: %i", c);
+}
+
