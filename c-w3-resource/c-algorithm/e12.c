e6699ba3d6cd631a328f6781ff007e738c20722d
Arnavgits <dwivediarnav20@gmail.com>
2024-07-19 15:36:08 +0000

    Fri, Jul 19, 2024, 9:06 PM +05:30

--- c-w3-resource/c-algorithm/e12.c
+++ c-w3-resource/c-algorithm/e12.c
@@ -6,7 +6,7 @@ int mai(void)
     int a = get_int("a: ");
     int b = get_int("b: ");
     int c = a + b;
-    if((a <= 20 && a >= 10) || (b <= 20 && b >= 10))
+    if((c <= 20 && c >= 10))
     {
         printf("30\n");
     }
