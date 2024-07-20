ac673bc789a04eb1233f8c38bb7c7bf6508247b1
Arnavgits <dwivediarnav20@gmail.com>
2024-07-20 08:35:32 +0000

    Sat, Jul 20, 2024, 2:05 PM +05:30

--- c-w3-resource/c-array/e3.c
+++ c-w3-resource/c-array/e3.c
@@ -1,23 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int mai(void)
+int main(void)
 {
     int a = get_int("array elements to enter: ");
     int b[a];
 
     for(int i = 0; i < a; i++)
     {
-        b[i] = get_int("element %i: ")
+        b[i] = get_int("element %i: ", i);
     }
 
     for(int i = 0;i < a; i++)
     {
         for(int j = 0;j < a; j++)
         {
-            if(i != j && b[i] = b[j])
+            if(i != j && b[i] != b[j])
             {
-                
+                printf("%i  ", b[i] );
             }
         }
     }
