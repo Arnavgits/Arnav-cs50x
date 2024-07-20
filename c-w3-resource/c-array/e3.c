aae74da7a189ac956e9d5a8016b3c9eb8b977e06
Arnavgits <dwivediarnav20@gmail.com>
2024-07-20 08:29:47 +0000

    Sat, Jul 20, 2024, 1:59 PM +05:30

---
+++ c-w3-resource/c-array/e3.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int mai(void)
+{
+    int a = get_int("array elements to enter: ");
+    int b[a];
+
+    for(int i = 0; i < a; i++)
+    {
+        b[i] = get_int("element %i: ")
+    }
+    
+}
