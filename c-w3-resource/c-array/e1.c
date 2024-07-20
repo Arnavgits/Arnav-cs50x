b4588148e287412d2b74227fd16161691eef512f
Arnavgits <dwivediarnav20@gmail.com>
2024-07-20 06:27:15 +0000

    Sat, Jul 20, 2024, 11:57 AM +05:30

--- c-w3-resource/c-array/e1.c
+++ c-w3-resource/c-array/e1.c
@@ -5,9 +5,12 @@ int main(void)
 {
     int a = get_int("no of elements in array: ");
     int b[a];
+    int c = 0;
 
     for(int  i = 0; i < a; i++)
     {
-        b[i] = get_int("array element %i: ");
+        b[i] = get_int("array element %i: ", i);
+        c = c + b[i];
     }
+    printf("sum: %i\n", c);
 }
