ebc7438b802d08ec474bd630abe94d8ec53526ca
Arnavgits <dwivediarnav20@gmail.com>
2024-07-20 07:16:40 +0000

    Sat, Jul 20, 2024, 12:46 PM +05:30

--- c-w3-resource/c-array/e2.c
+++ c-w3-resource/c-array/e2.c
@@ -6,15 +6,20 @@ int main(void)
     // get array elements from user
     int a = get_int("Array elements to enter: ");
     int b[a];
+    int c = 0;
 
     for (int i = 0; i < a; i++)
     {
         b = get_int("element %i: ");
     }
-
-
-    //apply condition to check for duplicate elemets
-
+    //apply condition to check for duplicate elements
+    for(int i = 0;i < a; i++)
+    {
+        if(b[i + 1] < a && b[i] == b[i + 1])
+        {
+            c
+        }
+    }
 
     //print the no of duplicate elements
 }
