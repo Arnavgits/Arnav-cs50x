a290559c5cafcd301eaa72a36f48d4b2f9ea13d4
Arnavgits <dwivediarnav20@gmail.com>
2024-07-20 07:13:55 +0000

    Sat, Jul 20, 2024, 12:43 PM +05:30

--- c-w3-resource/c-array/e2.c
+++ c-w3-resource/c-array/e2.c
@@ -4,7 +4,13 @@
 int main(void)
 {
     // get array elements from user
-    
+    int a = get_int("Array elements to enter: ");
+    int b[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        b = get_int("element %i: ");
+    }
 
 
     //apply condition to check for duplicate elemets
