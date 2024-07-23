d465097b67866bf8bf3341a7099d2c6058813661
Arnavgits <dwivediarnav20@gmail.com>
2024-07-23 15:38:36 +0000

    Tue, Jul 23, 2024, 9:08 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -149,7 +149,7 @@ void tabulate(void)
         {
         if(candidates[i].eliminated == false)
         {
-            candidates[preferences[i][j]]++;
+            candidates[preferences[i][j]].votes++;
         }
         }
     }
