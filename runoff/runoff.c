929f48e56dd0e4401ae1804b469ab7312dfead36
Arnavgits <dwivediarnav20@gmail.com>
2024-07-24 08:15:39 +0000

    Wed, Jul 24, 2024, 1:45 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -194,10 +194,7 @@ bool is_tie(int min)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        for(int j = 0; j < candidate_count; j++)
-        {
-        if( i != j && candidates[i].eliminated == false && candidates[j].eliminated == false && candidates)
-        }
+        if()
     }
     return false;
 }
