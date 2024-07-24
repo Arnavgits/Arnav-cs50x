143b021e9adc82962247d26c8a25202a07545fff
Arnavgits <dwivediarnav20@gmail.com>
2024-07-24 07:48:39 +0000

    Wed, Jul 24, 2024, 1:18 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -179,8 +179,8 @@ int find_min(void)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        
-        if( candidates[i].eliminated == false && )
+        int d = 0;
+        if( candidates[i].eliminated == false && candidates[i].votes < candidates[i + 1].votes )
     }
     return 0;
 }
