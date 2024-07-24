9b1294b0572710d404e09d811bc627ed816da118
Arnavgits <dwivediarnav20@gmail.com>
2024-07-24 10:05:24 +0000

    Wed, Jul 24, 2024, 3:35 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,11 +181,11 @@ int find_min(void)
     int min_votes = voter_count;
     for(int i = 0; i < candidate_count ; i++)
     {
-        if(candidates[i].eliminated == false && )
+        if(candidates[i].eliminated == false && candidates[i].votes < min_votes)
         {
+            min_votes = candidates[i].votes;
         }
     }
-    return d;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
