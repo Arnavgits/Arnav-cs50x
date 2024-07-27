407b001d2c59c69dd40dd8786b98c16f29a26389
Arnavgits <dwivediarnav20@gmail.com>
2024-07-27 09:20:37 +0000

    Sat, Jul 27, 2024, 2:50 PM +05:30

--- week-4/swap.c
+++ week-4/swap.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
+void swap(int *c, int *d);
+
 int main(void)
 {
-    int *a = 2;
-    int *b = 3;
+    int a = 2;
+    int b = 3;
 
-    printf("a is %i and b is %i", a, b);
+    printf("a is %i and b is %i\n", a, b);
+    //passing by reference
     swap(&a , &b);
-    printf("a is %i and b is %i", a, b);
+    printf("a is %i and b is %i\n", a, b);
 }
 
+//passing by reference
 void swap(int *c, int *d)
 {
     int e = *c;
     *c = *d;
-    
-
-
+    *d = e;
 }
