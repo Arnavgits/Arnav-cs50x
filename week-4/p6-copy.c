9e6c1b5a38c58db30a163fc251f070483a625be6
Arnavgits <dwivediarnav20@gmail.com>
2024-07-27 08:17:46 +0000

    Sat, Jul 27, 2024, 1:47 PM +05:30

---
+++ week-4/p6-copy.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *a = get_string("a: ");
+
+    char *b = malloc(strlen(a) + 1);
+
+    for(int i = 0, n = strlen(a); i < n; i++)
+    {
+        a[i] = b[i];
+    }
+
+    a[0] = toupper(a[0]);
+
+    printf("%s", a);
+    printf("%s", b);
+}
