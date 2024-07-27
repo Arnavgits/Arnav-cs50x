899178ef628567e552a31c19dd5c0bef2760d258
Arnavgits <dwivediarnav20@gmail.com>
2024-07-27 09:13:25 +0000

    Sat, Jul 27, 2024, 2:43 PM +05:30

--- week-4/swap.c
+++ week-4/swap.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
+    int *a = 2;
+    int *b = 3;
+
+    printf("a is %i and b is %i", a, b);
+    swap(&a , &b);
+    printf("a is %i and b is %i", a, b);
+}
+
+void swap(int *c, int *d)
+{
     
 }
