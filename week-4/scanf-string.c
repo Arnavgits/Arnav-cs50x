529cc63139045a5baa31a6795ca6020fe90a3073
Arnavgits <dwivediarnav20@gmail.com>
2024-07-27 09:34:02 +0000

    Sat, Jul 27, 2024, 3:04 PM +05:30

--- week-4/scanf-string.c
+++ week-4/scanf-string.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     char *a = malloc(4);
+    if(a == NULL)
+    {
+        return 1;
+    }
+
     printf("a: ");
-    scanf("%c", a);
+    scanf("%s", a);
     printf("%s\n", a);
+    free(a);
+    return 0;
 }
