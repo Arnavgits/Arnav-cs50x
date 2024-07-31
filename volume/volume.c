6223c134ba82f084c0f583767ba98e0df38a68f3
Arnavgits <dwivediarnav20@gmail.com>
2024-07-31 14:28:56 +0000

    Wed, Jul 31, 2024, 7:58 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t a[];
-    fread(a , sizeof(int) ,  )
+    fread(a , HEADER_SIZE , 1, input );
+    fwrite(a, HEADER_SIZE, 1, output );
 
     // TODO: Read samples from input file and write updated data to output file
 
