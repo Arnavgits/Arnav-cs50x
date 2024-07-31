ac9b5a0506a59f012c34537fd069dab4ef137c84
Arnavgits <dwivediarnav20@gmail.com>
2024-07-31 15:29:51 +0000

    Wed, Jul 31, 2024, 8:59 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -34,9 +34,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t a[44];
+    uint8_t a[43];
     fread(a , HEADER_SIZE , 1, input );
-    fwrite(output, HEADER_SIZE, 1, a );
+    fwrite(a, HEADER_SIZE, 1, output );
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t b;
