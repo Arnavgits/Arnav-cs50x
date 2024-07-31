0f1d98a38fddf6bef25ccc2a8765505ae97c4175
Arnavgits <dwivediarnav20@gmail.com>
2024-07-31 18:08:11 +0000

    Wed, Jul 31, 2024, 11:38 PM +05:30

--- c-w3-resource/basic/e29.c
+++ c-w3-resource/basic/e29.c
@@ -6,11 +6,22 @@ int main(void)
     scanf("%i ", a);
 
     int number[a];
+    int b = 0;
+    int c = 0;
 
     for(int i = 0; i < a, i++)
     {
         printf("Enter %i number: ");
         scanf("%i ", a[i]);
+
+        if(a[i] > 0)
+        {
+            b++;
+            c += a[i];
+        }
     }
+    int d = c / b;
+    printf("No of positive numbers: %i", b);
+    printf("Average of all positive numbers: %i", );
 
 }
