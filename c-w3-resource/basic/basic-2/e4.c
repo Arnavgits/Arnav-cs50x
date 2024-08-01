24ccc6caeb8c2ecce5b746e1f099e72469bb481c
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 15:39:46 +0000

    Thu, Aug 1, 2024, 9:09 PM +05:30

--- c-w3-resource/basic/basic-2/e4.c
+++ c-w3-resource/basic/basic-2/e4.c
@@ -2,16 +2,10 @@
 
 int main(void)
 {
-    int a,b,c;
+    int a[3];
 
-    printf("Enter 1st no: ");
-    scanf("%i", a);
-
-    printf("Enter 2nd no: ");
-    scanf("%i", b);
-
-    printf("Enter 3rd no: ");
-    scanf("%i", c);
-
-    
+    for (int i = 0; i < 3; i++)
+    {
+        printf("")
+    }
 }
