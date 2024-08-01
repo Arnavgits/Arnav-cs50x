e234e51c4cb105211412267272ccc4e34c52db5e
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 08:58:58 +0000

    Thu, Aug 1, 2024, 2:28 PM +05:30

--- c-w3-resource/basic/e43.c
+++ c-w3-resource/basic/e43.c
@@ -3,18 +3,20 @@
 int main(void)
 {
     int a,b;
+    int c;
 
-    printf("Input no of lines:")
-    scanf("%i", a);
+    printf("Input no of lines:");
+    scanf("%i", &a);
 
-    printf("Input no of characters:")
-    scanf("%i", b);
+    printf("Input no of characters:");
+    scanf("%i", &b);
 
     for (int i = 1; i <= a; i++)
     {
-        for (int j = 1; j <= b; j++)
+        for (int j = c; j <= b; j++)
         {
-            printf("%i", )
+            printf("%i", c );
+            c = j;
         }
         printf("\n");
     }
