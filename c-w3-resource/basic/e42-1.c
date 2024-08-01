29893b8dc317da0e821081aca43da23fc0779daa
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 08:31:28 +0000

    Thu, Aug 1, 2024, 2:01 PM +05:30

--- c-w3-resource/basic/e42-1.c
+++ c-w3-resource/basic/e42-1.c
@@ -4,10 +4,10 @@ int main(void)
 {
     int a;
     printf("Input no of lines: ");
-    scanf("%i", a);
+    scanf("%i", &a);
 
-    for (int i = 0; i <= a; i++)
+    for (int i = 1; i <= a; i++)
     {
-        
+        printf("%i %i %i\n", i, power(i , 2), power(i , 3));
     }
 }
