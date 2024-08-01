f652f2838f0f15d559ae3fa6e4e6d1f09214879a
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 07:36:52 +0000

    Thu, Aug 1, 2024, 1:06 PM +05:30

--- c-w3-resource/basic/e29.c
+++ c-w3-resource/basic/e29.c
@@ -4,15 +4,15 @@ int main(void)
 {
     int a;
     printf("hOW MANY NUMBERS TO ENTER: ");
-    scanf("%i ", a);
+    scanf("%i ", &a);
 
     int number[a];
     int b = 0;
     int c = 0;
 
-    for(int i = 0; i < a, i++)
+    for(int i = 0; i < a; i++)
     {
-        printf("Enter %i number: ");
+        printf("Enter %i number: ", i + 1);
         scanf("%i ", &number[i]);
 
         if(number[i] > 0)
