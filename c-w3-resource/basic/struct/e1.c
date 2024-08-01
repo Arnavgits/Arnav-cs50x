12c32c6ca9285e168a5336c9a31409fedd4a12e8
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 17:12:47 +0000

    Thu, Aug 1, 2024, 10:42 PM +05:30

--- c-w3-resource/basic/struct/e1.c
+++ c-w3-resource/basic/struct/e1.c
@@ -5,9 +5,19 @@ typedef struct
     char name[100];
     int age;
     int Total_marks;
-}Student;
+}Students;
 
 int main(void)
 {
+    students student1,student2;
+
+    student1.name[0] = 'Aditya';
+    student1.age = 19;
+    student1.Total_marks = 50;
+
+    student2.name[1] = 'priyam';
+    student2.age = 20;
+    student2.Total_marks = 80;
+
     
 }
