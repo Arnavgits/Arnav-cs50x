7f356799236c23207c1f8a0d8a44838f47fb0fed
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 09:22:52 +0000

    Thu, Aug 1, 2024, 2:52 PM +05:30

--- c-w3-resource/basic/basic-2/e1.c
+++ c-w3-resource/basic/basic-2/e1.c
@@ -4,7 +4,13 @@ int main(void)
 {
     int a;
     printf("No of integers to enter: ");
-    scanf("%i", a);
+    scanf("%i", &a);
 
     int b[a];
+
+    for ( int i = 0; i < a; i++)
+    {
+        printf("Enter %i element: ", i + 1);
+        scanf("%i", &b[i]);
+    }
 }
