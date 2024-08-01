7a3dfcf69c955415f07ef8a9eee1f6315a0ba4ab
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 08:52:58 +0000

    Thu, Aug 1, 2024, 2:22 PM +05:30

--- c-w3-resource/basic/e43.c
+++ c-w3-resource/basic/e43.c
@@ -12,7 +12,7 @@ int main(void)
 
     for (int i = 1; i <= a; i++)
     {
-        for (int j = 1; j <= b; j++)
+        for (int j = i; j <= b; j++)
         {
             printf("%i", )
         }
