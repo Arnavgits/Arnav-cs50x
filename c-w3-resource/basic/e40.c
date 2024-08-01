bf5faaf4381e5552482eecb7b0163c42fc544db9
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 08:12:42 +0000

    Thu, Aug 1, 2024, 1:42 PM +05:30

--- c-w3-resource/basic/e40.c
+++ c-w3-resource/basic/e40.c
@@ -9,5 +9,11 @@ int main(void)
     printf("Input second number: ");
     scanf("%i", &b);
 
-    
+    for (int i = a; i < b; i++)
+    {
+        if(i % 7 == 2 || i % 7 == 3)
+        {
+            printf("%i\n", i);
+        }
+    }
 }
