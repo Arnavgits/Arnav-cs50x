0e2a805835e82a64437afdd6300bfe405c206187
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 09:05:52 +0000

    Thu, Aug 1, 2024, 2:35 PM +05:30

--- c-w3-resource/basic/e43.c
+++ c-w3-resource/basic/e43.c
@@ -11,12 +11,8 @@ int main(void)
     printf("Input no of characters:");
     scanf("%i", &b);
 
-    for (int i = 1; i <= a; i++)
+    for (int i = 0; i < a; i++)
     {
-        for (int j = c; j <= 6 * i; j++)
-        {
-            printf("%i", j );
-        }
-        printf("\n");
+        
     }
 }
