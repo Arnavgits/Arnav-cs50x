6a5e4a7408ed749661bf2903c2540e883ca46aae
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 07:57:02 +0000

    Thu, Aug 1, 2024, 1:27 PM +05:30

--- c-w3-resource/basic/e34.c
+++ c-w3-resource/basic/e34.c
@@ -5,20 +5,20 @@ int main(void)
     int a,b;
     int c = 0;
     printf("Input first number: ");
-    scanf("%i", a);
+    scanf("%i", &a);
 
     printf("Input second number: ");
-    scanf("%i", b);
+    scanf("%i", &b);
 
     printf("List of odd numbers:");
     for (int i = a + 1; i < b; i++)
     {
         if( i % 2 != 0)
         {
-            printf("%i", i);
+            printf("%i\n", i);
             c += i;
         }
     }
-    printf("Sum: %i", c);
+    printf("Sum: %i\n", c);
 
 }
