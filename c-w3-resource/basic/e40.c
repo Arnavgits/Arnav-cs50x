116fa9fe241a9fe2ebc3c06b06eb5c55cb71087e
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 08:09:13 +0000

    Thu, Aug 1, 2024, 1:39 PM +05:30

---
+++ c-w3-resource/basic/e40.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    printf("Input first number: ");
+    scanf("%i", &a);
+
+    printf("Input second number: ");
+    scanf("%i", &b);
+
+    
+}
