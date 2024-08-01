a644c34785d9048c25996f543b0a224508316203
Arnavgits <dwivediarnav20@gmail.com>
2024-08-01 17:23:42 +0000

    Thu, Aug 1, 2024, 10:53 PM +05:30

--- c-w3-resource/basic/struct/e1.c
+++ c-w3-resource/basic/struct/e1.c
@@ -10,20 +10,25 @@ typedef struct
 int main(void)
 {
     students student1,student2;
+    float a;
 
      printf("Name:");
      scanf("%s", student1.name);
      printf("age:");
-     scanf("%s", student1.age);
+     scanf("%i", student1.age);
      pritnf("Total marks:");
-     scanf("%s", student1.Total_marks);
+     scanf("%i", student1.Total_marks);
 
      printf("Name:");
      scanf("%s", student2.name);
      printf("age:");
-     scanf("%s", student2.age);
+     scanf("%i", student2.age);
      pritnf("Total marks:");
-     scanf("%s", student2.Total_marks);
+     scanf("%i", student2.Total_marks);
 
+     a = (student1.Total_marks + student2.Total_marks) / 2;
+
+     printf("%s\n%i\n%i\naverage: %f", student1.name, student1.age, student1.Total_marks);
      
+
 }
