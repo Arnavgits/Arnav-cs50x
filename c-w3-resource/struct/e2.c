ed4d4a3e2af7f7d62917fb6daa3be985fb105f7c
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 10:19:34 +0000

    Fri, Aug 2, 2024, 3:49 PM +05:30

--- c-w3-resource/struct/e2.c
+++ c-w3-resource/struct/e2.c
@@ -19,14 +19,11 @@ int main(void)
     printf("Enter Student 2 Time(Hours, min ,seconds): ");
     scanf("%i %i %i", &student2.hours, &student2.minutes, &student2.seconds);
 
-    Total_hours = student1.hours + student2.hours;
-    Total_minutes = student1.minutes + student2.minutes;
     Total_seconds = student1.seconds + student2.seconds;
+    Total_minutes = student1.minutes + student2.minutes + Total_seconds / 60;
+    Total_hours = student1.hours + student2.hours + Total_minutes / 60;
 
-        Total_minutes = Total_minutes + Total_seconds / 60;
         Total_seconds = Total_seconds % 60;
-
-        Total_hours = Total_hours + Total_minutes / 60;
         Total_minutes = Total_minutes % 60;
 
     printf("Resultant Time: %i : %i : %i\n", Total_hours, Total_minutes, Total_seconds);
