5a42d72a1059fec04eb1bec1feecc6489012853f
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 11:32:43 +0000

    Fri, Aug 2, 2024, 5:02 PM +05:30

--- c-w3-resource/struct/e3.c
+++ c-w3-resource/struct/e3.c
@@ -23,18 +23,21 @@ int main(void)
     printf("Book3 (title, author, price): ");
     scanf("%s %s %i", book3.title, book3.author, &book3.price);
 
+    int max_book_price = max(book1.price, book2.price, book3.price);
+    int min_book_price = min(book1.price, book2.price, book3.price);
 
-    if(max(book1.price, book2.price, book3.price) == book1.price)
+
+    if(max_book_price == book1.price)
     printf("Max Book:%s , %s, %i\n", book1.title, book1.author, book1.price);
-    else if(max(book2.price, book1.price, book3.price) == book2.price)
+    else if(max_book_price == book2.price)
     printf("Max Book:%s , %s, %i\n", book2.title, book2.author, book2.price);
     else
     printf("Max Book:%s , %s, %i\n", book3.title, book3.author, book3.price);
 
 
-    if(min(book1.price, book2.price, book3.price) == book1.price)
+    if(min_book_price == book1.price)
     printf("Min Book:%s , %s, %i\n", book1.title, book1.author, book1.price);
-    else if(min(book2.price, book1.price, book3.price) == book2.price)
+    else if(min_book_price == book2.price)
     printf("Min Book:%s , %s, %i\n", book2.title, book2.author, book2.price);
     else
     printf("Min Book:%s , %s, %i\n", book3.title, book3.author, book3.price);
@@ -44,14 +47,18 @@ int max(int x, int y, int z)
 {
     if(x > y && x > z)
     return x;
+    else if(y > x && y > z)
+    return y;
     else
-    return 1;
+    return z;
 }
 
 int min(int x, int y, int z)
 {
     if(x < y && x < z)
     return x;
+    else if(y < x && y < z)
+    return y;
     else
-    return 1;
+    return z;
 }
