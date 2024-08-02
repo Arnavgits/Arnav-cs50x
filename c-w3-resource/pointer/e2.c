4ad0725d678ff1c454b9dcf279edcb398ac24325
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 12:31:54 +0000

    Fri, Aug 2, 2024, 6:01 PM +05:30

--- c-w3-resource/pointer/e2.c
+++ c-w3-resource/pointer/e2.c
@@ -3,6 +3,17 @@
 int main(void)
 {
     int m = 29;
+
+    printf("Address of m:%p", &m);
+    printf("Value of m:%i", m);
+
     int *ab = &m;
-    
+
+    printf("Address of pointer ab: %p", &ab);
+    printf("Content of pointer ab: %i", *ab);
+
+    m = 34;
+    printf("Address of pointer ab: %p", &ab)
+    printf("Content of pointer ab: ")
+
 }
