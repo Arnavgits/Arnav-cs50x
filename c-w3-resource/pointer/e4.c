8ea2ec1e674652bea3b17180d468a6345e117975
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 11:57:37 +0000

    Fri, Aug 2, 2024, 5:27 PM +05:30

--- c-w3-resource/pointer/e4.c
+++ c-w3-resource/pointer/e4.c
@@ -3,10 +3,9 @@
 int main(void)
 {
     int m = 10;
-    int z = &m;
-    int *z = m;
+    int *z = &m;
 
-    printf("value of m:%i\naddress of m: %i\n")
+    printf("value of m:%i\naddress of m: %p\n", m, z);
 
 
 }
