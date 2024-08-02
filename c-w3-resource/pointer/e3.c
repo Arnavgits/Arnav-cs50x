88dde937295afef64a4e7f8543692bf9db05577c
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 12:44:36 +0000

    Fri, Aug 2, 2024, 6:14 PM +05:30

--- c-w3-resource/pointer/e3.c
+++ c-w3-resource/pointer/e3.c
@@ -2,5 +2,17 @@
 
 int main(void)
 {
-    
+    int m = 300;
+    float fx = 300.600006;
+    char cht = 'z';
+
+    printf("address of m = %p", &m);
+    printf("address of fx = %p", &fx);
+    printf("address of cht = %p", &cht);
+
+    int *a = &m;
+    int *b = &fx;
+    char *c = &cht;
+
+    printf("value at address of m = %")
 }
