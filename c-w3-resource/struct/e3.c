7f5b2b0ccec65597f0cfc462051dd197a3610626
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 10:52:09 +0000

    Fri, Aug 2, 2024, 4:22 PM +05:30

--- c-w3-resource/struct/e3.c
+++ c-w3-resource/struct/e3.c
@@ -5,7 +5,7 @@ typedef struct
     char title[100];
     char author[100];
     int price;
-}Book
+}Book;
 
 int main(void)
 {
@@ -20,21 +20,24 @@ int main(void)
     printf("Book3 (title, author, price): ");
     scanf("%s , %s, %i", &book3.title, &book3.author, &book3.price);
 
+
     if(max(book1.price, book2.price, book3.price))
     {
-    printf("%s , %s, %i", book1.title, book1.author, book1.price);
+    printf("%s , %s, %i\n", book1.title, book1.author, book1.price);
     }
     else if(max(book2.price, book1.price, book3.price))
-    printf("%s , %s, %i", book2.title, book2.author, book2.price);
+    printf("%s , %s, %i\n", book2.title, book2.author, book2.price);
     else
-    printf("%s , %s, %i", book3.title, book3.author, book3.price);
+    printf("%s , %s, %i\n", book3.title, book3.author, book3.price);
+
 
     if(min(book1.price, book2.price, book3.price))
-    printf("%s , %s, %i", book1.title, book1.author, book1.price);
+    printf("%s , %s, %i\n", book1.title, book1.author, book1.price);
     else if(min(book2.price, book1.price, book3.price))
-    printf("%s , %s, %i", book2.title, book2.author, book2.price);
+    printf("%s , %s, %i\n", book2.title, book2.author, book2.price);
     else
-    printf("%s , %s, %i", book3.title, book3.author, book3.price);
+    printf("%s , %s, %i\n", book3.title, book3.author, book3.price);
+}
 
 int max(int x, int y, int z)
 {
