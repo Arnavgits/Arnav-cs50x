8757de5fa24922d4c8c4220780fcc62bb3841386
Arnavgits <dwivediarnav20@gmail.com>
2024-08-02 12:36:36 +0000

    Fri, Aug 2, 2024, 6:06 PM +05:30

--- c-w3-resource/pointer/e2.c
+++ c-w3-resource/pointer/e2.c
@@ -4,21 +4,21 @@ int main(void)
 {
     int m = 29;
 
-    printf("Address of m:%p", &m);
-    printf("Value of m:%i", m);
+    printf("Address of m:%p\n", &m);
+    printf("Value of m:%i\n", m);
 
     int *ab = &m;
 
-    printf("Address of pointer ab: %p", &ab);
-    printf("Content of pointer ab: %i", *ab);
+    printf("Address of pointer ab: %p\n", &ab);
+    printf("Content of pointer ab: %i\n", *ab);
 
     m = 34;
-    printf("Address of pointer ab: %p", &ab);
-    printf("Content of pointer ab: %i", *ab);
+    printf("Address of pointer ab: %p\n", &ab);
+    printf("Content of pointer ab: %i\n", *ab);
 
-    ab = 7;
-    printf("Address of m: %p", &m);
-    printf("Value of m: %i", );
+    *ab = 7;
+    printf("Address of m: %p\n", &m);
+    printf("Value of m: %i\n", m );
 
 
 }
