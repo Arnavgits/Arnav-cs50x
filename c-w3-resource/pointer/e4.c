7febe43d7b14d70d7abe2e98646ff70936f29b0d
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 04:05:08 +0000

    Sat, Aug 3, 2024, 9:35 AM +05:30

--- c-w3-resource/pointer/e4.c
+++ c-w3-resource/pointer/e4.c
@@ -2,18 +2,19 @@
 
 int main(void)
 {
-    int a,b;
+    int a,b,c;
 
     printf("Input the first no: ");
-    scanf("%i", a);
+    scanf("%i", &a);
 
     printf("Input the second no: ");
-    scanf("%i", b);
+    scanf("%i", &b);
 
-
+    c = add(a , b);
+    printf("Sum: %i", c);
 }
 
-int add(int x , int y);
+int add(int *x , int *y);
 {
-    
+    return x + y;
 }
