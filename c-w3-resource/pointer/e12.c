664d3ad3c26ecb1d5ebf3e494934d960b742421d
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 13:46:13 +0000

    Sat, Aug 3, 2024, 7:16 PM +05:30

--- c-w3-resource/pointer/e12.c
+++ c-w3-resource/pointer/e12.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
+int factorial(int *x);
+
 int main(void)
 {
     int a;
     printf("Input a number: ");
     scanf("%i", &a);
 
-    int b = factorial(a);
+    int b = factorial(&a);
+    printf("%i", b);
 }
 
 int factorial(int *x)
 {
-    if (x == 1)
+    if (*x == 1)
     {
-        return x;
+        return *x;
     }
     else
     (
-        return x * factorial(x - 1);
+        return *x * factorial(x - 1);
     )
 }
 
