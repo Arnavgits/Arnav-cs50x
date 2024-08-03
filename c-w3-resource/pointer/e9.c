c8a72fa7037d312d6d0c75094eb2a24b28714025
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 06:11:33 +0000

    Sat, Aug 3, 2024, 11:41 AM +05:30

--- c-w3-resource/pointer/e9.c
+++ c-w3-resource/pointer/e9.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
-    printf("")
+    int a;
+    printf("No of elements to enter: ");
+    scanf("%i", a);
+
+    number[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("Number %i:", i + 1);
+        scanf("%i", number[i]);
+    }
 }
