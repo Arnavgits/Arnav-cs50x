2118394becf4d0a20138fdede14808cae9d81b59
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 13:42:55 +0000

    Sat, Aug 3, 2024, 7:12 PM +05:30

--- c-w3-resource/pointer/e12.c
+++ c-w3-resource/pointer/e12.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
-    
+    int a;
+    printf("Input a number: ");
+    scanf("%i", &a);
+
+    int b = factorial(a);
 }
+
+int factorial(int x)
+{
+    if (x == 1)
+    {
+        return x;
+    }
+    else
+    (
+        return x * factorial(x - 1);
+    )
+}
+
