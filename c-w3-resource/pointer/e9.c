b801e151f826a6f58f17ecc84954c284d6135356
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 06:24:50 +0000

    Sat, Aug 3, 2024, 11:54 AM +05:30

--- c-w3-resource/pointer/e9.c
+++ c-w3-resource/pointer/e9.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     int a;
     int d = 0;
     printf("No of elements to enter: ");
-    scanf("%i", a);
+    scanf("%i", &a);
 
     int *b = malloc(sizeof(int) * a);
 
     for (int i = 0; i < a; i++)
     {
         printf("Number %i:", i + 1);
-        scanf("%i", b[i]);
+        scanf("%i", &b[i]);
 
         if (b[i] > d)
         {
             d = b[i];
         }
     }
-
-    printf("Largest element: %i", d);
+    printf("Largest element: %i\n", d);
 }
