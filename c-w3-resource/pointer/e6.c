8dbeb6c6376d6712ae383ccbc58768021695c9c2
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 04:19:13 +0000

    Sat, Aug 3, 2024, 9:49 AM +05:30

--- c-w3-resource/pointer/e6.c
+++ c-w3-resource/pointer/e6.c
@@ -2,5 +2,21 @@
 
 int main(void)
 {
-    
+    int a,b;
+
+    printf("Input first no: ");
+    scanf("%i", &a);
+
+    printf("Input second no: ");
+    scanf("%i", &b);
+
+    int *c = &a;
+    int *d = &b;
+
+    if ( a == b)
+        return 1;
+    else if ( a > b)
+    printf("Max no is: %i", *c);
+    else
+    printf("Max no is: %i", *d);
 }
