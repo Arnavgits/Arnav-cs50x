c69169c09b45829c96d62f65472ef23318600f2f
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 04:01:13 +0000

    Sat, Aug 3, 2024, 9:31 AM +05:30

---
+++ c-w3-resource/pointer/e4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+
+    printf("Input the first no: ");
+    scanf("%i", a);
+
+    printf("Input the second no: ");
+    scanf("%i", b);
+
+    
+}
