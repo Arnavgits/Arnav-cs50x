07736cfafedaa10bf71cca20cb8b800c97466f5b
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 06:48:02 +0000

    Sat, Aug 3, 2024, 12:18 PM +05:30

--- c-w3-resource/pointer/e10.c
+++ c-w3-resource/pointer/e10.c
@@ -4,9 +4,17 @@ int main(void)
 {
     char str[100];
     char *a = str;
+    int b = 0;
 
     printf("Input a string: ");
     scanf("%99s", a);
 
-    for (int i = 0;i < )
+    for (int i = 0;i < 100; i++)
+    {
+        if(str[i] != 0)
+        {
+            b++;
+        }
+    }
+    printf("Size of input string: %i", b);
 }
