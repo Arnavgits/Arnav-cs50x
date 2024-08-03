1e2d5806c4477bbc9ad13ebdd2e8139aceca8ec8
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 07:01:02 +0000

    Sat, Aug 3, 2024, 12:31 PM +05:30

--- c-w3-resource/pointer/e11.c
+++ c-w3-resource/pointer/e11.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
-    
+    int a,b,c;
+    printf("Input the value of 1st element: ");
+    scanf("%i", a);
+
+    printf("Input the calue of 2nd element: ");
+    scanf("%i", b);
+
+    printf("Input the value of 3rd element: ");
+    scanf("%i", c);
+
+    printf("The value before swapping are")
 }
