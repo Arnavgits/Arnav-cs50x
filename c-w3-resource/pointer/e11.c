009a696c0d0dcdcd1702d4a3ba0b8277201fd37b
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 07:38:44 +0000

    Sat, Aug 3, 2024, 1:08 PM +05:30

--- c-w3-resource/pointer/e11.c
+++ c-w3-resource/pointer/e11.c
@@ -4,13 +4,13 @@ int main(void)
 {
     int a[3];
     printf("Input the value of 1st element: ");
-    scanf("%i", a[0]);
+    scanf("%i", &a[0]);
 
     printf("Input the calue of 2nd element: ");
-    scanf("%i", b[1]);
+    scanf("%i", &b[1]);
 
     printf("Input the value of 3rd element: ");
-    scanf("%i", c[2]);
+    scanf("%i", &c[2]);
 
     printf("The value before swapping are: ");
 
@@ -18,4 +18,13 @@ int main(void)
     {
         printf("element %i = %i", i + 1, a[i]);
     }
+
+    swap(a[0], a[1], a[2]);
+
+
+}
+
+int swap(int *x,int *y,int *z);
+{
+    
 }
