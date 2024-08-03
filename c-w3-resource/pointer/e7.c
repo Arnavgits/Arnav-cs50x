ed1b9d3f4f6d35050626930af5b2ad10462c9278
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 04:26:07 +0000

    Sat, Aug 3, 2024, 9:56 AM +05:30

--- c-w3-resource/pointer/e7.c
+++ c-w3-resource/pointer/e7.c
@@ -2,6 +2,17 @@
 
 int main(void)
 {
+    int a;
     printf("Input no of elements to enter in array: ");
-    
+    scanf("%i", a);
+
+    int b[a];
+
+    printf("Input 5 elements in array: ");
+
+    for(int i = 0; i < a; i++)
+    {
+        printf("element %i: ");
+        scanf("%i")
+    }
 }
