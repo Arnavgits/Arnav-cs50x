656d125724d78349ac5c29d2892487cdad40eb85
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 15:32:22 +0000

    Sat, Aug 3, 2024, 9:02 PM +05:30

--- c-w3-resource/pointer/e17.c
+++ c-w3-resource/pointer/e17.c
@@ -5,5 +5,16 @@ int main(void)
     int a;
     printf("No of elements to enter: ");
     scanf("%i", a);
-    
+    int b[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("element - %i: ", i + 1)
+        scanf("%i", &b[i]);
+    }
+
+    for (int i = 0; i < a; i++)
+    {
+        b[i] = b[a - i]
+    }
 }
