af5bf92e67d561866b806401b97780f00bf358fc
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 06:22:21 +0000

    Sat, Aug 3, 2024, 11:52 AM +05:30

--- c-w3-resource/pointer/e9.c
+++ c-w3-resource/pointer/e9.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     int a;
+    int d = 0;
     printf("No of elements to enter: ");
     scanf("%i", a);
 
@@ -12,7 +13,12 @@ int main(void)
     {
         printf("Number %i:", i + 1);
         scanf("%i", b[i]);
+
+        if (b[i] > d)
+        {
+            d = b[i];
+        }
     }
 
-
+    printf("Largest element: %i", d);
 }
