2d37fe4d2471c1c0f0c7341227540fe67e170d62
Arnavgits <dwivediarnav20@gmail.com>
2024-08-03 07:55:15 +0000

    Sat, Aug 3, 2024, 1:25 PM +05:30

--- c-w3-resource/pointer/e11.c
+++ c-w3-resource/pointer/e11.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+void swap(int *x,int *y,int *z);
+
 int main(void)
 {
     int a[3];
+    int i;
     printf("Input the value of 1st element: ");
     scanf("%i", &a[0]);
 
@@ -14,21 +17,25 @@ int main(void)
 
     printf("The value before swapping are: ");
 
-    for (int i = 0; i < 3; i++);
+    for ( i = 0; i < 3; i++)
     {
         printf("element %i = %i\n", i + 1, a[i]);
     }
 
-    swap(a[0], a[1], a[2]);
+    swap(&a[0], &a[1], &a[2]);
 
+    printf("After swapping: ");
 
+    for (int j = 0; j < 3; j++)
+    {
+        printf("element %i = %i\n", j + 1, a[j]);
+    }
 }
 
-int swap(int *x,int *y,int *z);
+void swap(int *x,int *y,int *z)
 {
     int tmp = *x;
     *x = *z;
     *z = *y;
     *y = tmp;
-    return *x,*y,*z;
 }
