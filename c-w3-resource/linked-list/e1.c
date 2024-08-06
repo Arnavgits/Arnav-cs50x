62b636a342c2806019a7f47c97d9d9b4fe29126c
Arnavgits <dwivediarnav20@gmail.com>
2024-08-06 16:06:42 +0000

    Tue, Aug 6, 2024, 9:36 PM +05:30

--- c-w3-resource/linked-list/e1.c
+++ c-w3-resource/linked-list/e1.c
@@ -10,17 +10,17 @@ typedef struct nod
 int main(void)
 {
     node *head = malloc(sizeof(node));
-    node *b = a;
+    node *b = head;
 
     b -> a = 1;
     b -> next = malloc(sizeof(node));
 
     b = b->next;
-    b->head = 2;
+    b->a = 2;
     b->next = malloc(sizeof(node));
 
     b = b->next;
-    b->head = 3;
+    b->a = 3;
     b->next = NULL;
 
     while (head != NULL)
