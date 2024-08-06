dc701fa631af166a059b66cd580f68223609b2dc
Arnavgits <dwivediarnav20@gmail.com>
2024-08-06 14:43:34 +0000

    Tue, Aug 6, 2024, 8:13 PM +05:30

--- c-w3-resource/linked-list/e1.c
+++ c-w3-resource/linked-list/e1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-typedef struct node
+typedef struct nod
 {
     int a;
-    
-}
+    node *next;
+}node;
 
 int main(void)
 {
+    node node1 = malloc(sizeof(node));
 
+    node1.a = 
 }
