14e6ab05df65679b79d38f1ccc3b38cf2aa7da14
Arnavgits <dwivediarnav20@gmail.com>
2024-08-07 12:19:57 +0000

    Wed, Aug 7, 2024, 5:49 PM +05:30

--- c-w3-resource/linked-list/e-2.c
+++ c-w3-resource/linked-list/e-2.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct nod
+{
+    int number;
+    struct nod *next;
+}node;
+
+int main(void)
+{
+    node *
+    int a;
+    printf("Input the number of nodes: ");
+    scanf("%i", a);
+
+
+}
