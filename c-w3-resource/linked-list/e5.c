31a0158b87ac54b5c1ce5268c64e7ffdbbf8f345
Arnavgits <dwivediarnav20@gmail.com>
2024-08-07 14:26:08 +0000

    Wed, Aug 7, 2024, 7:56 PM +05:30

--- c-w3-resource/linked-list/e5.c
+++ c-w3-resource/linked-list/e5.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct nod
+{
+    int number;
+    struct nod *next;
+}node;
+
 int main(void)
 {
-    
-}
+    node *head, *temp;
+    int a,b,new_data;
+    printf("Input the no of nodes: ");
+    scanf("%i", &a);
 
+    head = malloc(sizeof(node));
+    if (head == NULL)
+    {
+        printf("memory allocation failed\n");
+        return 1;
+    }
+    temp = head;
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("Input data for node %i: ", i + 1);
+        scanf("%i", &b);
+        temp -> number = b;
+
+        if ( i == a - 1)
+        {
+            temp -> next = NULL;
+        }
+        else
+        {
+            temp -> next = malloc(sizeof(node));
+            if ( temp -> next == NULL)
+            {
+                printf("memory allocation failed\n");
+                return 1;
+            }
+            temp = temp -> next;
+        }
+    }
+    printf("Data entered in the list are: \n");
+
+     temp = head;
+    for (int i = 0; i < a; i++)
+    {
+        printf("Data = %i\n", temp -> number);
+        temp = temp -> next;
+    }
+
+    printf("Input data to insert at the beginning of the list : ");
+    scanf("%i", &new_data);
+
+    temp = malloc(sizeof(node));
+    if (temp == NULL)
+    {
+        printf("memory allocation failed\n");
+        return 1;
+    }
+}
