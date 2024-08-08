84c85ae628dbfaad05553a135794bcad881a05ea
Arnavgits <dwivediarnav20@gmail.com>
2024-08-08 11:20:16 +0000

    Thu, Aug 8, 2024, 4:50 PM +05:30

--- c-w3-resource/linked-list/e6.c
+++ c-w3-resource/linked-list/e6.c
@@ -74,9 +74,9 @@ int main(void)
 
     for (int i = 1; i <= new_position; i++)
     {
-        if (i = new_position - 1)
+        if (i = new_position)
         {
-            f -> next = temp -> next;
+
         }
         temp = temp -> next;
     }
