df4a0ed83fe90e5b78fa9b0dae8d8c191d0e4502
Arnavgits <dwivediarnav20@gmail.com>
2024-08-09 12:17:12 +0000

    Fri, Aug 9, 2024, 5:47 PM +05:30

--- c-w3-resource/recursion/e2.c
+++ c-w3-resource/recursion/e2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+Total_sum(int x);
+
 int main(void)
+{
+    int a;
+    printf("Input the last number of the range starting from 1 : ");
+    scanf("%i", &a);
+
+    Total_sum(a);
+}
+
+void Total_sum(int x)
 {
     
 }
