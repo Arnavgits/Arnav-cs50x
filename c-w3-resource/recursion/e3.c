e8aace3f8c49d9be632fbbfe7951adbd8b2a7c88
Arnavgits <dwivediarnav20@gmail.com>
2024-08-09 12:35:41 +0000

    Fri, Aug 9, 2024, 6:05 PM +05:30

--- c-w3-resource/recursion/e3.c
+++ c-w3-resource/recursion/e3.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+void fibon(a);
+
 int main(void)
 {
     int a;
     printf("Input number of terms for the series :");
     scanf("%i", a);
 
+    printf("The series are : \n")
     fibon(a);
+}
+
+void fibon(a)
+{
+    
 }
