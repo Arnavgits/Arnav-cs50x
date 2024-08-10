1ba3851f70ab9de4f8f5336d5192c84f82fd4d34
Arnavgits <dwivediarnav20@gmail.com>
2024-08-10 08:59:33 +0000

    Sat, Aug 10, 2024, 2:29 PM +05:30

--- c-w3-resource/callback-function/e1.c
+++ c-w3-resource/callback-function/e1.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+
+void square(int brr[]);
 
 int main(void)
 {
     int arr[] = {1,2,3,4,5,6};
+    for (int i = 0; i < 6; i++ )
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
     square(arr);
 
 }
 
-void square(int x)
+void square(int brr[])
 {
-    
+    for (int i = 0; i < 6; i++)
+    {
+        int b = pow(brr[i], 2);
+        printf("%i ", b);
+    }
+    printf("\n");
 }
