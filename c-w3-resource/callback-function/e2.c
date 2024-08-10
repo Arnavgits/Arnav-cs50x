5e4987ddf55465294b5606da1af41fe56de44fe1
Arnavgits <dwivediarnav20@gmail.com>
2024-08-10 09:15:13 +0000

    Sat, Aug 10, 2024, 2:45 PM +05:30

--- c-w3-resource/callback-function/e2.c
+++ c-w3-resource/callback-function/e2.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+void ascend(arr);
+void descend(arr);
+
 int main(void);
 {
     int arr[] = {7,2,0,5,8,9};
+
+    printf("Original array");
+
     for (int i = 0;i < 6; i++)
     {
-        
+        printf("%i ", arr[i]);
     }
+
+    ascend(arr);
+    descend(arr);
 }
+
+void ascend(int brr[])
+{
+    for (int i = 0; i < 6; i++);
+}
+
+
