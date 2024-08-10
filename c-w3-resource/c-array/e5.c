c5515a2118d1ef76d8ff900d73bea3557fc642a2
Arnavgits <dwivediarnav20@gmail.com>
2024-08-10 18:22:53 +0000

    Sat, Aug 10, 2024, 11:52 PM +05:30

---
+++ c-w3-resource/c-array/e5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+
+    printf("Input the number of elements to be stored in the array : ");
+    scanf("%i", &a);
+
+    printf("Input %i elements in the array : ", a);
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("Element %i : ");
+    }
+}
