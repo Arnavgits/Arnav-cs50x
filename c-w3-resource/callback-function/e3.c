97c9936701a2ae5c55c7fb38b90a9c6c92bc4034
Arnavgits <dwivediarnav20@gmail.com>
2024-08-11 07:45:41 +0000

    Sun, Aug 11, 2024, 1:15 PM +05:30

--- c-w3-resource/callback-function/e3.c
+++ c-w3-resource/callback-function/e3.c
@@ -6,21 +6,21 @@ int product(int arr[], int size);
 int main(void)
 {
     int a = 0;
-    int arr1[] = {10 ,20, 30, 40, 50, 60};
-    int size = sizeof(arr1) / sizeof(arr1[0]);
+    int arr[] = {10 ,20, 30, 40, 50, 60};
+    int size = sizeof(arr) / sizeof(arr[0]);
     printf("Original array: ");
 
     for (int i = 0;i < 6; i++)
     {
-        printf("%i ", arr1[i]);
+        printf("%i ", arr[i]);
     }
     printf("\n");
 
-    int sum = add(arr, size);
+    int sum = sum(arr, size);
     int product = multiply(arr, size);
 
-    printf("Sum: %i", sum);
-    printf("Product: %i", product);
+    printf("Sum: %i\n", sum);
+    printf("Product: %i\n", product);
 }
 
 int sum(int arr[], int size)
