e17726f37ba6ed7008f7b6dc466c15c9712d0485
Arnavgits <dwivediarnav20@gmail.com>
2024-08-11 08:06:41 +0000

    Sun, Aug 11, 2024, 1:36 PM +05:30

--- c-w3-resource/callback-function/e4.c
+++ c-w3-resource/callback-function/e4.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
     char * str[100];
-    
+    printf("String: ");
+    scanf("%s", &str);
+
+    int size = strlen(str);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (str(i) == str(size - i - 1))
+        {
+            
+        }
+    }
 }
