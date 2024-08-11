0e9ae99cf96c0a97e892f76be9b245cc61e0595e
Arnavgits <dwivediarnav20@gmail.com>
2024-08-11 07:43:56 +0000

    Sun, Aug 11, 2024, 1:13 PM +05:30

--- c-w3-resource/callback-function/e3.c
+++ c-w3-resource/callback-function/e3.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 
+int sum(int arr[], int size);
+int product(int arr[], int size);
+
 int main(void)
 {
     int a = 0;
     int arr1[] = {10 ,20, 30, 40, 50, 60};
     int size = sizeof(arr1) / sizeof(arr1[0]);
+    printf("Original array: ");
 
-    for (int i = 0; i < size - 1; i++)
+    for (int i = 0;i < 6; i++)
     {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (brr[j] > brr[j + 1])
-            {
-                int temp = brr[j + 1];
-                brr[j + 1] = brr[j];
-                brr[j] = temp;
-            }
-        }
-    }
-
-    printf("Ascending order : ");
-    for (int i = 0; i < size; i++)
-    {
-      printf("%i ", brr[i]);
+        printf("%i ", arr1[i]);
     }
     printf("\n");
+
+    int sum = add(arr, size);
+    int product = multiply(arr, size);
+
+    printf("Sum: %i", sum);
+    printf("Product: %i", product);
+}
+
+int sum(int arr[], int size)
+{
+    int ctr = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        ctr += arr[i];
+    }
+    return ctr;
+}
+
+int product(int arr[], int size)
+{
+    int ctr = 0;
+
+        for (int i = 0; i < size; i++)
+    {
+        ctr *= arr[i];
+    }
+    return ctr;
 }
