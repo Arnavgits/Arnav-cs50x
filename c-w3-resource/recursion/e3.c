4f4a2c8ed4219f965c4a94bdbb4ed85e2fb15109
Arnavgits <dwivediarnav20@gmail.com>
2024-08-12 15:23:31 +0000

    Mon, Aug 12, 2024, 8:53 PM +05:30

--- c-w3-resource/recursion/e3.c
+++ c-w3-resource/recursion/e3.c
@@ -12,9 +12,15 @@ int main(void)
 
 void fibon(int a)
 {
-    int b = 1;
+    int b = 0;
+    int c = 1;
+    printf("The series are: \n");
+    
     for (int i = 1; i <= a; i++)
     {
-        int sum = 
+        int sum = b + c;
+        printf("%i", sum);
+        b = c;
+        c = sum;
     }
 }
