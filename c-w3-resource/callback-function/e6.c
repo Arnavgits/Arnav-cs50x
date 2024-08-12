67f9b2b8fbca0e441a816154c04a238b9baf8f05
Arnavgits <dwivediarnav20@gmail.com>
2024-08-12 11:37:40 +0000

    Mon, Aug 12, 2024, 5:07 PM +05:30

---
+++ c-w3-resource/callback-function/e6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr1[] = {2 ,5 ,4 ,7 ,1 ,8 ,4 ,6 ,5 ,9 ,10 };
+    int size = arr[]/arr[0];
+    int select;
+
+    printf("Select an option:\n
+1. Calculate average of the said array elements:\n
+2. Calculate median of the said array elements:")
+}
