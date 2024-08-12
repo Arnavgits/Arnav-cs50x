b13ed40c9cc0008ecb2b3bc2bbc8a54d35d56102
Arnavgits <dwivediarnav20@gmail.com>
2024-08-12 17:31:42 +0000

    Mon, Aug 12, 2024, 11:01 PM +05:30

--- c-w3-resource/recursion/e4.c
+++ c-w3-resource/recursion/e4.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void print_recursion(char *arr2);
+
 int main(void)
+{
+     int a = get_int("array elements to enter: ");
+    int arr1[a];
+
+    for(int i = 0; i < a; i++)
+    {
+        arr1[i] = get_int("element %i: ", i);
+    }
+    printf("The elements in the array are: ");
+    print_recursion(arr1);
+}
+
+void print_recursion(char *arr2)
 {
     
 }
