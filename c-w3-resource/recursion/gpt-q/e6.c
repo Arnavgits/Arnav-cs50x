92f1a3d9c97a17cf36bb2e7022ba9b9e64cd408f
Arnavgits <dwivediarnav20@gmail.com>
2024-08-13 09:42:19 +0000

    Tue, Aug 13, 2024, 3:12 PM +05:30

--- c-w3-resource/recursion/gpt-q/e6.c
+++ c-w3-resource/recursion/gpt-q/e6.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int gcd(int x, int y);
+
 int main(void)
+{
+    int a,b;
+    printf("Enter 1st no:");
+    scanf("%i", &a);
+    printf("2nd no:");
+    scanf("%i", &b);
+
+    int result = gcd(a, b);
+}
+
+int gcd(int x, int y)
 {
     
 }
