e9f1887924656766a7d108e76ebd3a43eef86b39
Arnavgits <dwivediarnav20@gmail.com>
2024-08-13 09:28:42 +0000

    Tue, Aug 13, 2024, 2:58 PM +05:30

--- c-w3-resource/recursion/gpt-q/e5.c
+++ c-w3-resource/recursion/gpt-q/e5.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+int times(int x, int y);
+
 int main(void)
 {
-    
+    int number, power;
+    printf("Enter number: ");
+    scanf("%i", &number);
+    printf("Enter power: ");
+    scanf("%i", &power);
+
+    int result = times(number, power);
+    printf("Power: %i", result);
+}
+
+int times(int x, int y)
+{
+    if (y == 1)
+    return 1;
+
+    y--;
+    return x * times(x - 1,);
 }
