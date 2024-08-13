f8fa106a51e1ee4b44a35575cb99b295bec8c57b
Arnavgits <dwivediarnav20@gmail.com>
2024-08-13 11:18:50 +0000

    Tue, Aug 13, 2024, 4:48 PM +05:30

--- c-w3-resource/recursion/gpt-q/e7.c
+++ c-w3-resource/recursion/gpt-q/e7.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int count(int x);
+int count(int x, int y);
 
 int main(void)
 {
     int number;
     printf("Enter Number: ");
     scanf("%i", &number);
-    int digit = count(number);
+    int ctr = 0;
+    int digit = count(number, ctr);
 }
 
-int count(int x)
+int count(int x, int y)
 {
+    if (x / 10 == 0)
     
+    x % 10;
+    x / 10;
+    count(x / 10);
 }
