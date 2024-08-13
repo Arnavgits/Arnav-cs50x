c267366703021046ba4200d4a76caaf0bd97f6f3
Arnavgits <dwivediarnav20@gmail.com>
2024-08-13 09:17:03 +0000

    Tue, Aug 13, 2024, 2:47 PM +05:30

--- c-w3-resource/recursion/gpt-q/e4.c
+++ c-w3-resource/recursion/gpt-q/e4.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int add(int x);
+
 int main(void)
 {
-    
+    int a;
+    printf("Enter your number: ");
+    scanf("%i", &a);
+
+    int sum = add(a);
+}
+
+int add(int x)
+{
+    return x % 10 + return(add(x / 10));
+
 }
