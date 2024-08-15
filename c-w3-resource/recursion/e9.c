39f699f65b168ac475dbf17164667043f537357d
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 08:53:10 +0000

    Thu, Aug 15, 2024, 2:23 PM +05:30

--- c-w3-resource/recursion/e9.c
+++ c-w3-resource/recursion/e9.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+char *reverse_string(char *str3);
+
 int main(void)
 {
-    char str[100];
+    char str1[100],str2[100];
 
     printf("Input a string: ");
     scanf("%s", str);
 
+    str2 = reverse_string(str1);
+    printf("%s", str2);
+}
+
+char *reverse_string(char *str3)
+{
     
 }
