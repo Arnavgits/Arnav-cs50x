512627de41b4ce5f9d63f103a1b97f9cf39ae413
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 05:08:21 +0000

    Thu, Aug 15, 2024, 10:38 AM +05:30

--- c-w3-resource/file-handling/e2.c
+++ c-w3-resource/file-handling/e2.c
@@ -2,13 +2,15 @@
 
 int main(void)
 {
-    char str[100];
+    char str[100], temp;
     printf("Input file name to be opened: ");
     scanf("%s", str);
     FILE *ptr1 = fopen(str, "r");
 
     printf("The content of the %s file is: ", str);
-    fgetc()
 
+    while(temp == fgetc(ptr1) != EOF)
+    printf("%c", temp);
 
+    fclose(ptr1);
 }
