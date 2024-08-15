86f1aa9f5b28317ffc81c9236e96775eb9d7ecbc
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 07:16:47 +0000

    Thu, Aug 15, 2024, 12:46 PM +05:30

--- c-w3-resource/recursion/e7.c
+++ c-w3-resource/recursion/e7.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int gcd(int x , int y);
+
 int main(void)
 {
     int num1, num2;
@@ -8,4 +10,12 @@ int main(void)
     scanf("%i", &num1);
     printf("Input 2nd number: ");
     scanf("%i", num2);
+
+    int result = gcd(num1 , num2);
+    printf("GCD of %i and %i: %i", num1, num2, result);
+}
+
+int gcd(int x , int y)
+{
+    
 }
