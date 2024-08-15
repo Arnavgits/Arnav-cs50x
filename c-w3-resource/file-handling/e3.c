351f227fb8dd1df2336cba1feda4412dbf81a1c6
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 10:13:44 +0000

    Thu, Aug 15, 2024, 3:43 PM +05:30

--- c-w3-resource/file-handling/e3.c
+++ c-w3-resource/file-handling/e3.c
@@ -2,11 +2,17 @@
 
 int main(void)
 {
-    int lines;
+    FILE *ptr1 = fopen("test1.txt", "w");
+    int str[100],lines;
+
     printf("Input the number of lines to be written:");
     scanf("%i", lines);
 
-    
+    for (int i = 0; i < lines; i++)
+    {
+        gfets(ptr1, sizeof(str), stdin);
+        fprintf()
+    }
 
     printf("The lines are: \n");
 
