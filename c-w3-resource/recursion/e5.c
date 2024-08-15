6a0445890015373c8c63d563a701b0741d769400
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 06:34:33 +0000

    Thu, Aug 15, 2024, 12:04 PM +05:30

--- c-w3-resource/recursion/e5.c
+++ c-w3-resource/recursion/e5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int count(int x);
+int count(int x, int y);
 
 int main(void)
 {
@@ -9,10 +9,16 @@ int main(void)
     printf("Input a no: ");
     scanf("%i", number);
 
-    int result = count(number);
+    int ctr = 0;
+    int result = count(number, ctr);
 }
 
-int count(int x)
+int count(int x, int y)
 {
-    
+    if (x == 0)
+    return y;
+
+    x = x / 10;
+    y++
+    count(x, y);
 }
