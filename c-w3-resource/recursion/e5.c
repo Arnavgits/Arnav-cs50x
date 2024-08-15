605205542ea48ef435aa687773aa9d4ff130513f
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 06:31:45 +0000

    Thu, Aug 15, 2024, 12:01 PM +05:30

--- c-w3-resource/recursion/e5.c
+++ c-w3-resource/recursion/e5.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int count(int x);
+
 int main(void)
 {
     int number;
+
+    printf("Input a no: ");
+    scanf("%i", number);
+
+    int result = count(number);
+}
+
+int count(int x)
+{
+    
 }
