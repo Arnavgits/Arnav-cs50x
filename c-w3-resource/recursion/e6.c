f63f3e4b960d15896f592a24adad1aa52069b936
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 07:08:23 +0000

    Thu, Aug 15, 2024, 12:38 PM +05:30

--- c-w3-resource/recursion/e6.c
+++ c-w3-resource/recursion/e6.c
@@ -6,15 +6,18 @@ int main(void)
 {
     int num;
     printf("Input your number: ");
-    scanf("%i", num);
+    scanf("%i", &num);
 
     int ctr = 0;
     int result = add(num, ctr);
-    printf("Sum of digits : %i", result);
+    printf("Sum of digits : %i\n", result);
 }
 
 int add(int x, int y)
 {
+    if (x == 0)
+    return y;
+
     y += x % 10;
     x = x / 10;
 
