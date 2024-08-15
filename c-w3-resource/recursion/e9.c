7cf31e929a112d7224dc68e97f3ccdcb21e17dc0
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 09:18:08 +0000

    Thu, Aug 15, 2024, 2:48 PM +05:30

--- c-w3-resource/recursion/e9.c
+++ c-w3-resource/recursion/e9.c
@@ -14,22 +14,23 @@ int main(void)
     int size = strlen(str1);
 
     reverse_string(str1, size - 1, 0);
+    printf("%s", str1);
 }
 
 void reverse_string(char *str3, int length,int ctr)
 {
     if (ctr == length)
     {
-        return str3;
+        return;
     }
-    swap(str3[ctr], str3[length]);
+    swap(&str3[ctr], &str3[length]);
 
-    return(str3, length - 1, ctr + 1);
+    reverse_string(str3, length - 1, ctr + 1);
 }
 
 void swap(char *x, char *y)
 {
-    char temp = x;
+    char *temp = x;
     x = y;
     y = temp;
 }
