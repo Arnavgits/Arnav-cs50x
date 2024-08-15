a1df8cf1e0e49dfeb3dd609d4dadbe7a09ce1d7b
Arnavgits <dwivediarnav20@gmail.com>
2024-08-15 08:06:57 +0000

    Thu, Aug 15, 2024, 1:36 PM +05:30

--- c-w3-resource/recursion/e8.c
+++ c-w3-resource/recursion/e8.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+int largest(int arr2, int length);
+
 int main(void)
+{
+    int size;
+   printf("Size of array: ");
+   scanf("%i", size);
+
+   int arr1[size];
+   printf("Input %i elements in the array:", size);
+
+   for (int i = 0; i < size ; i++)
+   {
+    printf("element - %i:\n", i);
+    scanf("%i", arr1[i]);
+   }
+
+   int result = largest(arr1, size);
+}
+
+int largest(int arr2, int length)
 {
     
 }
