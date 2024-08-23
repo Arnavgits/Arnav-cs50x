c2ad102768f616ce51fd9651c0f8f295eb7fb095
Arnavgits <dwivediarnav20@gmail.com>
2024-08-23 15:38:15 +0000

    Fri, Aug 23, 2024, 9:08 PM +05:30

--- c-w3-resource/file-handling/e4.c
+++ c-w3-resource/file-handling/e4.c
@@ -2,5 +2,11 @@
 
 int main(void)
 {
-    
+    char str1[100];
+    printf("Input file name to be opened: ");
+    scanf("%s", &str1);
+
+    printf("The content of the file %s", str1);
+
+    FILE * ptr1 = fopen(str1, "");
 }
