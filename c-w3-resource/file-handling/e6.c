320f7013a6d006bf31c558811b5f93e45a6b143b
Arnavgits <dwivediarnav20@gmail.com>
2024-08-23 17:00:18 +0000

    Fri, Aug 23, 2024, 10:30 PM +05:30

--- c-w3-resource/file-handling/e6.c
+++ c-w3-resource/file-handling/e6.c
@@ -2,5 +2,22 @@
 
 int main(void)
 {
-    
+    int ctr = 0;
+    char name[20];
+    printf("Input file name to be opened: ");
+    scanf("%s", name);
+
+    FILE * ptr1 = fopen(name , "r");
+
+    char ch;
+    while((ch = fgetc(ptr1)) != EOF)
+    {
+        printf("%c", ch);
+        if(ch == '\n')
+        {
+            ctr++;
+        }
+    }
+    printf("Number of lines: %i\n", ctr);
+    fclose(ptr1);
 }
