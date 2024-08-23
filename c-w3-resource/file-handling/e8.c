cfc7f3434dbd389db2aeffebbda10083355dea12
Arnavgits <dwivediarnav20@gmail.com>
2024-08-23 18:04:02 +0000

    Fri, Aug 23, 2024, 11:34 PM +05:30

--- c-w3-resource/file-handling/e8.c
+++ c-w3-resource/file-handling/e8.c
@@ -2,5 +2,20 @@
 
 int main(void)
 {
-    
+    char name[20];
+    int line;
+    int i = 0;
+    printf("Input the file name to be opened: ");
+    scanf("%s", name);
+
+    printf("Input the line to be removed: ");
+    scanf("%i", line);
+
+    FILE * ptr1 = fopen(name, "w");
+
+    char ch;
+    while((ch = fgetc(ptr1)) != EOF)
+    {
+        
+    }
 }
