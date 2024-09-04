3a3c1a5722ff3cbc1cd7636226f57c631a65edd9
Arnavgits <dwivediarnav20@gmail.com>
2024-09-04 13:52:26 +0000

    Wed, Sep 4, 2024, 7:22 PM +05:30

--- python/pynative/e1.py
+++ python/pynative/e1.py
@@ -2,6 +2,6 @@ number1 = int(input("number1: "))
 number2 = int(input("number2: "))
 
 if number1 * number2 <= 1000:
-    return number1 * number2
+    print(number1 * number2)
 else:
-    return number1 + number2
+    print(number1 + number2)
