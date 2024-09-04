559110bffe1daef6ccefb134fca4684fbc0a8ea9
Arnavgits <dwivediarnav20@gmail.com>
2024-09-04 13:58:28 +0000

    Wed, Sep 4, 2024, 7:28 PM +05:30

--- python/pynative/e2.py
+++ python/pynative/e2.py
@@ -1,9 +1,8 @@
 current = 0
 previous = 0
 
-sum = current + previous
-
 for i in range(10):
+    sum = current + previous
     print(f"Current number {current} Previous number {previous} Sum: {sum}")
     previous = current
     current += 1
