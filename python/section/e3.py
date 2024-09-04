17b074d01d3f953e4b3e69e8a30ea4ebd975f4a2
Arnavgits <dwivediarnav20@gmail.com>
2024-09-04 11:44:17 +0000

    Wed, Sep 4, 2024, 5:14 PM +05:30

--- python/section/e3.py
+++ python/section/e3.py
@@ -5,4 +5,7 @@ books = []
 with open("b1.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        print(row)
+        books.append(row)
+
+for book in books:
+    print(book)
