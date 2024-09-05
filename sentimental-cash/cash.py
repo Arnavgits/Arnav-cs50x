69dab1fec916dfafd5b1ae62670338f0f1979b79
Arnavgits <dwivediarnav20@gmail.com>
2024-09-05 15:27:19 +0000

    Thu, Sep 5, 2024, 8:57 PM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,7 +1,8 @@
 from cs50 import get_float
 
  # coins of 25 - quarter ,10 - dime ,5 - nickel ,1 - penny units
-amount = get_float("Amount: ")
+ while True:
+    amount = get_float("Amount: ")
 
 quarter = 0
 dime = 0
