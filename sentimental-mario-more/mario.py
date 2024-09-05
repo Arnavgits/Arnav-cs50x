c4e925c5f1a65e9d6d0b01a38b9ad285cbd23335
Arnavgits <dwivediarnav20@gmail.com>
2024-09-05 14:40:08 +0000

    Thu, Sep 5, 2024, 8:10 PM +05:30

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -9,7 +9,7 @@ while True:
 for i in range(height):
 
     for j in range(height - i - 1):
-        print(" ", end = "")
+        print("  ", end = "")
 
     for k in range(i + 1):
         print("#", end = "")
