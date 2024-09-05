9f4b880b107495f709745fc1a374785ffff916dc
Arnavgits <dwivediarnav20@gmail.com>
2024-09-05 07:51:31 +0000

    Thu, Sep 5, 2024, 1:21 PM +05:30

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,6 +4,6 @@ for i in range(hashes):
     for i in range(hashes - 1):
         print(' ')
         ctr = 0
-        ctr += 1
+        ctr += 1 and ctr <= hashes
     for i in range(ctr):
         print("#", end ='')
