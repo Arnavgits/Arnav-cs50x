6be624b279e0f617305de09a599cac0aca57e1e1
Arnavgits <dwivediarnav20@gmail.com>
2024-09-05 07:59:49 +0000

    Thu, Sep 5, 2024, 1:29 PM +05:30

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,9 +3,11 @@ hashes = int(input("Enter no of hashes: "))
 ctr = 0
 ctr2 = hashes
 for i in range(hashes):
+    print()
     ctr += 1 and ctr <= hashes
     ctr2 -= 1
     for i in range(ctr2):
         print(' ', end = '')
     for i in range(ctr):
-        print("#", end ='')
+        print("#", end = '')
+
