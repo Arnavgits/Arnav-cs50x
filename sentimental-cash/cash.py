27a5a9740460198833aa34051f41bfe532fe14c9
Arnavgits <dwivediarnav20@gmail.com>
2024-09-05 15:01:45 +0000

    Thu, Sep 5, 2024, 8:31 PM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,6 +1,12 @@
 from cs50 import get_float
 
- # coins of 25,10,5,1 units
+ # coins of 25 - quarter ,10 - dime ,5 - nickel ,1 - penny units
 amount = get_float("Amount: ")
 
+quarter = 0
+dime = 0
+nickel = 0
+penny = 0
 
+while True:
+    
