565f646667e7ece4c1cd07c2b83f6f716b625c51
Arnavgits <dwivediarnav20@gmail.com>
2024-09-06 19:09:12 +0000

    Sat, Sep 7, 2024, 12:39 AM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,6 +3,8 @@ while True:
     number = int(input("Credit card number: "))
     remainder = number % 100
     ctr = ctr + (remainder/10)
+    number = number / 100
+    if number 
 
 
 # american express , master card, visa
