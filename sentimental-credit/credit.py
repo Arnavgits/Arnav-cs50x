65ee85aa67754387ecdde68d27cceac5f5e46e37
Arnavgits <dwivediarnav20@gmail.com>
2024-09-06 19:08:24 +0000

    Sat, Sep 7, 2024, 12:38 AM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,8 +1,8 @@
+ctr = 0
 while True:
     number = int(input("Credit card number: "))
-    number % 100
+    remainder = number % 100
+    ctr = ctr + (remainder/10)
+
 
 # american express , master card, visa
-
-if number[1] == 3 and number[2] == 4 :
-    print("american express")
