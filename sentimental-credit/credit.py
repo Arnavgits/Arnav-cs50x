405ec170d5d7f29ca91dd77923b0312465cee71b
Arnavgits <dwivediarnav20@gmail.com>
2024-09-06 19:40:52 +0000

    Sat, Sep 7, 2024, 1:10 AM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,7 +4,7 @@ while True:
 
     remainder = number % 100
     left = 2 * remainder / 10
-    if left > 10:
+    if left > 9:
         ctr = left/10 + left % 10
     else:
         ctr = ctr + left
