4753f336a42f30750c8c06aa4561466e8e933196
Arnavgits <dwivediarnav20@gmail.com>
2024-09-07 08:22:59 +0000

    Sat, Sep 7, 2024, 1:52 PM +05:30

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,7 +3,11 @@ from cs50 import get_string
 str = get_string("Text: ")
 
 Total_letters = 0
+Total_sentences = 0
 
+for char in str:
+    if char == " ":
+        Total_letters += 1
 
-
-
+    if char == "." or char == "!" or char == "?":
+        Total_sentences += 1
