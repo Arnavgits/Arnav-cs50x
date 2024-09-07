89dfa3b9cb1cbcc8497e6cccc1766e30610ca1f6
Arnavgits <dwivediarnav20@gmail.com>
2024-09-07 08:31:43 +0000

    Sat, Sep 7, 2024, 2:01 PM +05:30

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -12,6 +12,14 @@ for char in str:
     if char == "." or char == "!" or char == "?":
         Total_sentences += 1
 
+L = Total_letters/100
+S = Total_sentences/100
+
 Index_number = 0.0588 * L - 0.296 * S - 15.8
 
-
+if Index_number >= 16:
+    print("Grade 16+")
+elif Index_number < 1:
+    print("Before Grade 1")
+else:
+    print(f"Grade {Index_number}")
