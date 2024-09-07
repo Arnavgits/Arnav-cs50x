28e3c92a9f91c2464a7e5a302efb60eeca078216
Arnavgits <dwivediarnav20@gmail.com>
2024-09-07 08:42:07 +0000

    Sat, Sep 7, 2024, 2:12 PM +05:30

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -13,7 +13,8 @@ for char in str:
     if char == "." or char == "!" or char == "?":
         sentences += 1
 
-    if char != " "
+    if char != " " and char != "?" and char != "!":
+        letters += 1
 
 L = (letters/words) * 100
 S = (sentences/words) * 100
