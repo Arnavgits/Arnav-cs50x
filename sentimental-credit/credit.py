0de9441cab761a61dd2cba9fcaf3a070cfb624eb
Arnavgits <dwivediarnav20@gmail.com>
2024-09-07 07:24:40 +0000

    Sat, Sep 7, 2024, 12:54 PM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,10 +1,35 @@
 from cs50 import get_int
 
 
-
-ctr = 0
-while True:
+def main():
     number = get_int("Credit card number: ")
+
+    if valid_card(number):
+        card_type = get_type(number)
+    else:
+        print("INVALID")
+
+
+def valid_card(number):
+
+sum = 0
+ctr = 0
+
+while number > 0:
+
+    last_digit = int(number/10)
+
+    if ctr % 2 == 0:
+        sum += last_digit
+    else:
+        doubled = 2 * last_digit
+        if doubled > 9:
+            sum += int(doubled/10) + doubled % 10
+        else:
+            sum += doubled
+            
+
+
 
     while number > 0:
         remainder = number % 100
