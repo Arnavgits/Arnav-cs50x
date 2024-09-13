8fdd5046424f36405c1d7668ddf47b38fab518a0
Arnavgits <dwivediarnav20@gmail.com>
2024-09-13 09:25:16 +0000

    Fri, Sep 13, 2024, 2:55 PM +05:30

--- movies/9.sql
+++ movies/9.sql
@@ -1,9 +1,6 @@
 SELECT DISTINCT people.name
-FROM people
-JOIN stars
-ON stars.person_id = people.id
-
-JOIN movies
-ON movies.id = stars.movie_id
+FROM stars
+JOIN movies ON stars.movie_id = movies.id
+JOIN people on stars.person_id = people.id
 WHERE movies.year = 2004
 ORDER BY people.birth;
