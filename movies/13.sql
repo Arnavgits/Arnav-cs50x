cfea4719a823224380a077ab7397adc8b586a825
Arnavgits <dwivediarnav20@gmail.com>
2024-09-13 11:56:12 +0000

    Fri, Sep 13, 2024, 5:26 PM +05:30

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,6 @@
 SELECT name FROM people JOIN stars ON people.id = stars.person_id
   JOIN movies ON stars.movie_id = movies.id
  WHERE movies.id IN (
- SELECT id FROM movies JOIN stars ON movies.id = stars.movie_id
+ SELECT movies.id FROM movies JOIN stars ON movies.id = stars.movie_id
  JOIN people ON stars.person_id = people.id
  WHERE people.name = 'Kevin Bacon' AND people.birth = '1958') AND people.name !=  'Kevin Bacon';
