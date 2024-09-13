648f8e14f4ac973eb895c463ed892c10b133d020
Arnavgits <dwivediarnav20@gmail.com>
2024-09-13 06:38:17 +0000

    Fri, Sep 13, 2024, 12:08 PM +05:30

---
+++ movies/2.sql
@@ -0,0 +1,3 @@
+In 2.sql, write a SQL query to determine the birth year of Emma Stone.
+Your query should output a table with a single column and a single row (not counting the header) containing Emma Stone’s birth year.
+You may assume that there is only one person in the database with the name Emma Stone.
