8ff2993e9cbda7e9dbc8a84823ec3815b3f8cd33
Arnavgits <dwivediarnav20@gmail.com>
2024-09-14 18:37:45 +0000

    Sun, Sep 15, 2024, 12:07 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- looking for row in crime_scene_reports when duck was stolen--
-SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = 'Humphrey Street';
+SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = 'Humphrey Street' AND description LIKE "%duck%";
 --| 295 | 2023 | 7     | 28  | Humphrey Street | Theft of the CS50 duck took place at 10:15am
  --at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were--
  --present at the time – each of their interview transcripts mentions the bakery.--
- 
+
