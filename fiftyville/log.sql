c90403b58a0bfe4c3882b2201682ae6df71e5dfd
Arnavgits <dwivediarnav20@gmail.com>
2024-09-20 10:26:50 +0000

    Fri, Sep 20, 2024, 3:56 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -26,4 +26,4 @@ AND activity ="exit";
 --name - Vanessa , phone_number - (725) 555-4692, passsport_number - 2963008352--
 
 SELECT * FROM phone_calls JOIN people ON people.phone_number = phone_calls.caller JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-   ...> WHERE bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND bakery_security_logs.activity = "exit" AND phone_calls.duration < 60;
+   ...> WHERE bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND phone_calls.duration < 60;
