89a3997a96eee61bde61e16259bf6e9b5d84067a
Arnavgits <dwivediarnav20@gmail.com>
2024-09-20 07:12:53 +0000

    Fri, Sep 20, 2024, 12:42 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,7 +10,7 @@ SELECT * FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%thie
 --| 162 | Eugene  | 2023 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.--
 --| 163 | Raymond | 2023 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket.--
 
-SELECT * FROM bakery_security_logs WHERE day = 28 AND hour = 10 AND activity = 'exit';
+SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND hour = 10 AND activity = 'exit';
 --9 rows; license_plate--
 
 SELECT * FROM atm_transactions WHERE month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
