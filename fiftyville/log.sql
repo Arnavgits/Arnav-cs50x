114ec1169060889b46596463f074101cc866aa42
Arnavgits <dwivediarnav20@gmail.com>
2024-09-20 10:24:30 +0000

    Fri, Sep 20, 2024, 3:54 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -25,5 +25,5 @@ AND activity ="exit";
 --After joining people and bakery_security-logs I get--
 --name - Vanessa , phone_number - (725) 555-4692, passsport_number - 2963008352--
 
-SELECT * FROM phone_calls JOIN people ON people.phone_number = phone_calls.receiver JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+SELECT * FROM phone_calls JOIN people ON people.phone_number = phone_calls.caller JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
    ...> WHERE bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND bakery_security_logs.activity = "exit" AND phone_calls.duration < 60;
