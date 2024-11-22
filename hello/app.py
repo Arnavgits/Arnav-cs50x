f548b28f27c68c231d3d99b7f6d0e210fc5f7319
Arnavgits <dwivediarnav20@gmail.com>
2024-11-22 08:05:12 +0000

    Fri, Nov 22, 2024, 1:35 PM +05:30

--- hello/app.py
+++ hello/app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 @app.route("/", method = ["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name", "world")
-        return render_template("greet.html", name=name)
+        return render_template("greet.html", name=request.form.get("name", "world"))
     return render_template("index.html")
