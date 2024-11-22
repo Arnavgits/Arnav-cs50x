dcac13f855375dde510d3fde9dc68c05b3f94d4b
Arnavgits <dwivediarnav20@gmail.com>
2024-11-22 07:22:58 +0000

    Fri, Nov 22, 2024, 12:52 PM +05:30

--- hello/app.py
+++ hello/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/greet")
+def greet():
     name = request.args.get("name", "world")
     return render_template("index.html", name=name)
