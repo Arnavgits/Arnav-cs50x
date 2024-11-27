783230534ff040fc4567e2507f3e228404bb51f3
Arnavgits <dwivediarnav20@gmail.com>
2024-11-27 10:13:16 +0000

    Wed, Nov 27, 2024, 3:43 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -39,6 +39,7 @@ def index():
 
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
+        print(liDct)
 
         return render_template("index.html", birthdays=birthdays)
 
