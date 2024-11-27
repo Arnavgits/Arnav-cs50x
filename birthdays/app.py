04ff56a508829a353aafae82aaba19100f9f1df1
Arnavgits <dwivediarnav20@gmail.com>
2024-11-27 10:25:01 +0000

    Wed, Nov 27, 2024, 3:55 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,6 +30,7 @@ def index():
        name = request.form.get("name")
        month = request.form.get("month")
        day = request.form.get("day")
+       year = request.form.get("year")
        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
        return redirect("/")
 
