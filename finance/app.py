778478da9047031a838f5e2938255915f49c8229
Arnavgits <dwivediarnav20@gmail.com>
2024-12-02 17:04:31 +0000

    Mon, Dec 2, 2024, 10:34 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -39,7 +39,6 @@ def index():
 
         user_id = session["user_id"]
         symbol = db.execute("SELECT stock_symbol FROM transactions JOIN users ON transactions.user_id = users.id WHERE transactions.user_id = ?", user_id)
-        stock = lookup(symbol)
         shares = db.execute("SELECT shares FROM ")
 
 @app.route("/buy", methods=["GET", "POST"])
