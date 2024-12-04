2ac293f0e58a49788da6ccbd355040e954265e63
Arnavgits <dwivediarnav20@gmail.com>
2024-12-04 17:01:53 +0000

    Wed, Dec 4, 2024, 10:31 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -84,12 +84,11 @@ def history():
     user_id = session["user_id"]
     if request.method == "POST":
         # RETRIEVE stock_symbol, price, no. of shares, date and time
-        row = db.execute("SELECT stock_symbol, price, shares, time, status WHERE user_id = ?", user_id)
+        rows = db.execute("SELECT stock_symbol, price, shares, time, status WHERE user_id = ?", user_id)
 
-        db.execute("UPDATE transactions SET status = CASE WHEN ? > 0 THEN 'Bought' WHEN ? < 0 THEN 'Sold' ELSE 'invalid'", row["shares"], row["shares"])
+        db.execute("UPDATE transactions SET status = CASE WHEN ? > 0 THEN 'Bought' WHEN ? < 0 THEN 'Sold' ELSE 'invalid'", rows["shares"], rows["shares"])
 
-
-
+        return render_template("history.html", rows=rows)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
